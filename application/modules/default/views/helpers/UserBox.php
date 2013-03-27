4eaf85689561f4cf0acc40630ef7ea0b4f7a47e5
Adam Kudrna <adam@adamkudrna.cz>
2013-03-27 11:42:43 +0100

    Added icons to user box

--- application/modules/default/views/helpers/UserBox.php
+++ application/modules/default/views/helpers/UserBox.php
@@ -19,9 +19,9 @@ class Zend_View_Helper_UserBox extends Zend_View_Helper_Abstract {
         . '<div class="dropdown pull-right">'
         . '<a href="#" role="button" class="btn btn-icon dropdown-toggle" id="userDropdown" data-toggle="dropdown"><i class="icon-angle-down"></i></a>'
         . '<ul class="dropdown-menu" role="menu" aria-labelledby="userDropdown">'
-        . '<li><a href="' . $this->view->url(array('controller' => 'user', 'action' => 'inputlist'), 'default', true) . '">Alle meine Beiträge ansehen</a></li>';
+        . '<li><a href="' . $this->view->url(array('controller' => 'user', 'action' => 'inputlist'), 'default', true) . '"><i class="icon-list"></i> Alle meine Beiträge ansehen</a></li>';
       if ($identity->lvl == 'adm' || $identity->lvl == 'edt') {
-          $html.= '<li><a href="' . $this->view->baseUrl() . '/admin">Zum Adminbereich</a></li>';
+          $html.= '<li><a href="' . $this->view->baseUrl() . '/admin"><i class="icon-cog"></i> Zum Adminbereich</a></li>';
         }
       $html .= '<li><a href="' . $this->view->url(array('controller' => 'user', 'action' => 'logout'), 'default', true) . '"><i class="icon-signout"></i> Logout</a></li>'
         . '</ul>'
