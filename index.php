eda8532cdb204668253f7fe5f2f102f819eef856
Markus Hackel <mha@xima.de>
2013-05-07 14:47:02 +0200

    Moved contents of folder "public" to project root directory
    Reason: possible installation in subfolder of shared hosting without
    changing the web server config

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 // Define path to application directory
 defined('APPLICATION_PATH')
-    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/application'));
 
 // Define application environment
 defined('APPLICATION_ENV')
--- readme.txt
+++ readme.txt
@@ -1,4 +1,1 @@
 # Readme
-Das ist ein Test von DMI
-
-und das ist einer von MHA
