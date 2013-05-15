616d6fcddb6ef36db2bfeb35f73c917a02f616d1
Markus Hackel <mha@xima.de>
2013-05-15 15:50:11 +0200

    Changed email charset to UTF-8

--- application/models/Emails.php
+++ application/models/Emails.php
@@ -219,7 +219,7 @@ class Model_Emails extends Model_DbjrBase {
       $success = true;
     } else {
       // E-Mail verschicken
-      $mail = new Zend_Mail();
+      $mail = new Zend_Mail('UTF-8');
       $mail->setBodyText($message);
       $mail->setFrom($senderEmail, $senderName);
       $mail->addTo($receiver);
