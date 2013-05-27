f86fc6ad08ecc5d8fa17b643d1cb427b23421fce
Markus Hackel <mha@xima.de>
2013-05-27 15:12:21 +0200

    Fixed merging issue

--- application/models/Inputs.php
+++ application/models/Inputs.php
@@ -492,7 +492,6 @@ class Model_Inputs extends Model_DbjrBase {
     $rowSet = $this->fetchAll($select);
     if (count($rowSet) > 0) {
       $return = true;
-      $this->_flashMessenger->addMessage('Vielen Dank! Deine Beiträge wurden bestätigt!', 'success');
       foreach ($rowSet as $row) {
         $row->user_conf = ($reject ? 'r' : 'c');
         $row->confirm_key = '';
