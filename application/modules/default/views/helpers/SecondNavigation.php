ee9ffc63762af8b7625124be0ccac245f88a97cc
Markus Hackel <mha@xima.de>
2013-05-28 08:48:01 +0200

    Added consideration of show follow-up flag

--- application/modules/default/views/helpers/SecondNavigation.php
+++ application/modules/default/views/helpers/SecondNavigation.php
@@ -16,7 +16,7 @@ class Zend_View_Helper_SecondNavigation extends Zend_View_Helper_Abstract {
       'question' => false,
       'input' => ($nowDate->isEarlier($con->inp_fr)),
       'voting' => ($nowDate->isEarlier($con->vot_fr) || $nowDate->isLater($con->vot_to)),
-      'follow-up' => (!$nowDate->isLater($con->vot_to)),
+      'follow-up' => (!$nowDate->isLater($con->vot_to) || $con->follup_show == 'n'),
     );
     $items = array(
       'article' => array(
