eccff1afb1053e9190859cb1575ba033925cb6e4
Marco Dinnbier <dinnbier@digitalroyal.de>
2013-09-13 09:52:22 +0200

    popup:resizable & scrollable, fixes for ie

--- application/modules/admin/views/helpers/Popuplink.php
+++ application/modules/admin/views/helpers/Popuplink.php
@@ -6,9 +6,9 @@
  */
 class Admin_View_Helper_Popuplink extends Zend_View_Helper_Abstract {
   
-  public function Popuplink( $url , $title = 'Neues Fenster', $width = 200, $height = 200, $text = 'Fenster öffnen' , $linktitle = 'Popup öffnen', $classes = '') {
+  public function Popuplink( $url , $title = 'Neues_Fenster', $width = 200, $height = 200, $text = 'Fenster öffnen' , $linktitle = 'Popup öffnen', $classes = '') {
    
-    $onclick = "window.open('$url','$title','width=$width,height=$height, scrollbars=yes');return false;";  
+    $onclick = "window.open('$url','$title','width=$width,height=$height,resizable=yes,scrollbars=yes');return false;";  
       
     $html = '<a href="#" title="'.$linktitle.'" onclick="'.$onclick.'" class="'.$classes.'" >'.$text.'</a>';
    
