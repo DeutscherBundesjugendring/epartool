57bc958737aef0c1fb9b3494cfbfe5187f6b59a2
Martin Bohal <mb@mbohal.cz>
2013-11-02 11:07:16 +0100

    DBJR-29: added article preview functionality

--- application/modules/admin/controllers/ArticleController.php
+++ application/modules/admin/controllers/ArticleController.php
@@ -53,91 +53,20 @@ class Admin_ArticleController extends Zend_Controller_Action {
     }
 
     public function createAction() {
-        $kid = $this->getRequest()->getParam('kid', 0);
-        $consultation = null;
-        $form = null;
-        $consultationModel = new Model_Consultations();
-        $refNameModel = new Model_ArticleRefNames();
-        $consultation = $consultationModel->getById($kid);
-        $form = new Admin_Form_Article();
-
-        $form->setAction($this->view->baseUrl() . '/admin/article/create/kid/' . $kid);
-        $multiOptions = array(0 => 'Bitte auswählen...');
-        if ($kid > 0) {
-            // set multiOptions for ref_nm
-            foreach ($refNameModel->getMultioptionsByType('b') as $key => $value) {
-                $multiOptions[$key] = $value;
-            }
-            $form->getElement('ref_nm')->setMultioptions($multiOptions);
-            $form->getElement('ref_nm')->setDescription('Bei Unterseiten gilt der Referenzname der Elternseite!');
-        } else {
-            // set multiOptions for ref_nm
-            foreach ($refNameModel->getMultioptionsByType('g') as $key => $value) {
-                $multiOptions[$key] = $value;
-            }
-            $form->getElement('ref_nm')->setMultioptions($multiOptions);
+        $isPreview = $this->getRequest()->getPost('preview');
+        if(!empty($isPreview)) {
+            $this->_forward('preview');
         }
-        $articleModel = new Model_Articles();
-        $firstLevelPages = $articleModel->getFirstLevelEntries($kid);
-        $parentOptions = array(
-            0 => 'Keine'
-        );
-        foreach ($firstLevelPages as $page) {
-            $parentOptions[$page['art_id']] = '[' . $page['art_id'] . '] ' . $page['desc'];
-        }
-        $form->getElement('parent_id')->setMultiOptions($parentOptions);
-        if ($this->getRequest()->isPost()) {
-            $data = $this->getRequest()->getPost();
-            $data = $this->setProject($data);
-            if ($form->isValid($data)) {
-                $articleModel = new Model_Articles();
-                $articleRow = $articleModel->createRow($form->getValues());
-                $articleRow->kid = $kid;
-                $articleRow->proj = implode(',', $data['proj']);
-                $newId = $articleRow->save();
-                if ($newId > 0) {
-                    $this->_flashMessenger->addMessage('Neuer Artikel wurde erstellt.', 'success');
-                } else {
-                    $this->_flashMessenger->addMessage('Erstellen des neuen Artikels fehlgeschlagen!', 'error');
-                }
-
-                $this->_redirect($this->view->url(array(
-                    'action' => 'index',
-                    'kid' => $kid
-                )), array('prependBase' => false));
-            } else {
-                $this->_flashMessenger->addMessage('Bitte prüfen Sie Ihre Eingaben!', 'error');
-                $form->populate($form->getValues());
-                $form->getElement('proj')->setValue($data['proj']);
-            }
-        }
-
-        foreach ($form->getElements() as $element) {
-            $element->clearFilters();
-            if ($element->getName() != 'proj') {
-                $element->setValue(html_entity_decode($element->getValue(), ENT_COMPAT, 'UTF-8'));
-            }
-        }
-
-        $this->view->assign(array(
-            'kid' => $kid,
-            'consultation' => $consultation,
-            'form' => $form
-        ));
-    }
-
-    public function editAction() {
-        $kid = $this->getRequest()->getParam('kid', 0);
-        $consultation = null;
-        $form = null;
-        $consultationModel = new Model_Consultations();
-        $refNameModel = new Model_ArticleRefNames();
-        $consultation = $consultationModel->getById($kid);
-        $aid = $this->getRequest()->getParam('aid', 0);
-        if ($aid > 0) {
-            $articleModel = new Model_Articles();
-            $articleRow = $articleModel->find($aid)->current();
+        else {
+            $kid = $this->getRequest()->getParam('kid', 0);
+            $consultation = null;
+            $form = null;
+            $consultationModel = new Model_Consultations();
+            $refNameModel = new Model_ArticleRefNames();
+            $consultation = $consultationModel->getById($kid);
             $form = new Admin_Form_Article();
+
+            $form->setAction($this->view->baseUrl() . '/admin/article/create/kid/' . $kid);
             $multiOptions = array(0 => 'Bitte auswählen...');
             if ($kid > 0) {
                 // set multiOptions for ref_nm
@@ -153,51 +82,146 @@ class Admin_ArticleController extends Zend_Controller_Action {
                 }
                 $form->getElement('ref_nm')->setMultioptions($multiOptions);
             }
+            $articleModel = new Model_Articles();
             $firstLevelPages = $articleModel->getFirstLevelEntries($kid);
             $parentOptions = array(
                 0 => 'Keine'
             );
             foreach ($firstLevelPages as $page) {
-                if ($page['art_id'] != $aid) {
-                    $parentOptions[$page['art_id']] = '[' . $page['art_id'] . '] ' . $page['desc'];
-                }
+                $parentOptions[$page['art_id']] = '[' . $page['art_id'] . '] ' . $page['desc'];
             }
             $form->getElement('parent_id')->setMultiOptions($parentOptions);
+            $isRetFromPreview = $this->getRequest()->getPost('backFromPreview');
+            if ($this->getRequest()->isPost() && empty($isRetFromPreview)) {
+                $data = $this->getRequest()->getPost();
+                $data = $this->setProject($data);
+                if ($form->isValid($data)) {
+                    $articleModel = new Model_Articles();
+                    $articleRow = $articleModel->createRow($form->getValues());
+                    $articleRow->kid = $kid;
+                    $articleRow->proj = implode(',', $data['proj']);
+                    $newId = $articleRow->save();
+                    if ($newId > 0) {
+                        $this->_flashMessenger->addMessage('Neuer Artikel wurde erstellt.', 'success');
+                    } else {
+                        $this->_flashMessenger->addMessage('Erstellen des neuen Artikels fehlgeschlagen!', 'error');
+                    }
 
-            if ($this->getRequest()->isPost()) {
-                // Formular wurde abgeschickt und muss verarbeitet werden
-                $params = $this->getRequest()->getPost();
-                $params = $this->setProject($params);
-                if ($form->isValid($params)) {
-                    $articleRow->setFromArray($form->getValues());
-                    $articleRow->proj = implode(',', $params['proj']);
-                    $articleRow->save();
-                    $this->_flashMessenger->addMessage('Änderungen wurden gespeichert.', 'success');
-                    $article = $articleRow->toArray();
-                    $article['proj'] = explode(',', $article['proj']);
-                } else {
-                    $this->_flashMessenger->addMessage('Bitte prüfen Sie Ihre Eingaben und versuchen Sie es erneut!', 'error');
-                    $article = $params;
+                    $this->_redirect($this->view->url(array(
+                        'action' => 'index',
+                        'kid' => $kid
+                    )), array('prependBase' => false));
                 }
-            } else {
-                $article = $articleModel->getById($aid);
-                $article['proj'] = explode(',', $article['proj']);
+                else {
+                    $this->_flashMessenger->addMessage('Bitte prüfen Sie Ihre Eingaben!', 'error');
+                    $form->populate($form->getValues());
+                    $form->getElement('proj')->setValue($data['proj']);
+                }
             }
-            $form->populate($article);
-        }
-
-        foreach ($form->getElements() as $element) {
-            $element->clearFilters();
-            if ($element->getName() != 'proj') {
-                $element->setValue(html_entity_decode($element->getValue(), ENT_COMPAT, 'UTF-8'));
+            elseif ($this->getRequest()->isPost() && !empty($isRetFromPreview)) {
+                $article = $this->getRequest()->getPost();
+                $article['proj'] = unserialize($article['proj']);
+                $form->populate($article);
             }
-        }
 
-        $this->view->assign(array(
-            'kid' => $kid,
-            'consultation' => $consultation,
-            'form' => $form
-        ));
+            foreach ($form->getElements() as $element) {
+                $element->clearFilters();
+                if ($element->getName() != 'proj') {
+                    $element->setValue(html_entity_decode($element->getValue(), ENT_COMPAT, 'UTF-8'));
+                }
+            }
+
+            $this->view->assign(array(
+                'kid' => $kid,
+                'consultation' => $consultation,
+                'form' => $form
+            ));
+        }
+    }
+
+    public function editAction() {
+        $isPreview = $this->getRequest()->getPost('preview');
+        if(!empty($isPreview)) {
+            $this->_forward('preview');
+        }
+        else {
+            $kid = $this->getRequest()->getParam('kid', 0);
+            $consultation = null;
+            $form = null;
+            $consultationModel = new Model_Consultations();
+            $refNameModel = new Model_ArticleRefNames();
+            $consultation = $consultationModel->getById($kid);
+            $aid = $this->getRequest()->getParam('aid', 0);
+            if ($aid > 0) {
+                $articleModel = new Model_Articles();
+                $articleRow = $articleModel->find($aid)->current();
+                $form = new Admin_Form_Article();
+                $multiOptions = array(0 => 'Bitte auswählen...');
+                if ($kid > 0) {
+                    // set multiOptions for ref_nm
+                    foreach ($refNameModel->getMultioptionsByType('b') as $key => $value) {
+                        $multiOptions[$key] = $value;
+                    }
+                    $form->getElement('ref_nm')->setMultioptions($multiOptions);
+                    $form->getElement('ref_nm')->setDescription('Bei Unterseiten gilt der Referenzname der Elternseite!');
+                } else {
+                    // set multiOptions for ref_nm
+                    foreach ($refNameModel->getMultioptionsByType('g') as $key => $value) {
+                        $multiOptions[$key] = $value;
+                    }
+                    $form->getElement('ref_nm')->setMultioptions($multiOptions);
+                }
+                $firstLevelPages = $articleModel->getFirstLevelEntries($kid);
+                $parentOptions = array(
+                    0 => 'Keine'
+                );
+                foreach ($firstLevelPages as $page) {
+                    if ($page['art_id'] != $aid) {
+                        $parentOptions[$page['art_id']] = '[' . $page['art_id'] . '] ' . $page['desc'];
+                    }
+                }
+                $form->getElement('parent_id')->setMultiOptions($parentOptions);
+                $isRetFromPreview = $this->getRequest()->getPost('backFromPreview');
+                if ($this->getRequest()->isPost() && empty($isRetFromPreview)) {
+                    // Formular wurde abgeschickt und muss verarbeitet werden
+                    $params = $this->getRequest()->getPost();
+                    $params = $this->setProject($params);
+                    if ($form->isValid($params)) {
+                        $articleRow->setFromArray($form->getValues());
+                        $articleRow->proj = implode(',', $params['proj']);
+                        $articleRow->save();
+                        $this->_flashMessenger->addMessage('Änderungen wurden gespeichert.', 'success');
+                        $article = $articleRow->toArray();
+                        $article['proj'] = explode(',', $article['proj']);
+                    } else {
+                        $this->_flashMessenger->addMessage('Bitte prüfen Sie Ihre Eingaben und versuchen Sie es erneut!', 'error');
+                        $article = $params;
+                    }
+                }
+                elseif ($this->getRequest()->isPost() && !empty($isRetFromPreview)) {
+                    $article = $this->getRequest()->getPost();
+                    $article['proj'] = unserialize($article['proj']);
+                }
+                else {
+                    $article = $articleModel->getById($aid);
+                    $article['proj'] = explode(',', $article['proj']);
+                }
+                $form->populate($article);
+            }
+
+            foreach ($form->getElements() as $element) {
+                $element->clearFilters();
+                if ($element->getName() != 'proj') {
+                    $element->setValue(html_entity_decode($element->getValue(), ENT_COMPAT, 'UTF-8'));
+                }
+            }
+
+            $this->view->assign(array(
+                'kid' => $kid,
+                'consultation' => $consultation,
+                'form' => $form
+            ));
+        }
     }
 
     public function deleteAction() {
@@ -216,6 +240,41 @@ class Admin_ArticleController extends Zend_Controller_Action {
             }
         }
         $this->_redirect('/admin/article/index/kid/' . $kid);
+    }
+
+    /**
+     * Shows a preview of an article as it would apper on the frontend.
+     * This method is allways only being used as a target of a _forward() method.
+     */
+    protected function previewAction()
+    {
+        if ($this->getRequest()->isPost()) {
+            $data = $this->getRequest()->getPost();
+            if(isset($data['preview'])) {
+                $articlePreviewForm = new Admin_Form_ArticlePreview();
+                $data['proj'] = serialize($data['proj']);
+                $articlePreviewForm->populate($data);
+
+                $prevView = new Zend_View();
+                $prevView->addScriptPath('application/modules/default/views/scripts');
+                $prevView->addHelperPath('application/modules/default/views/helpers');
+                $this->_helper->layout->setLayout('frontend');
+                $this->_helper->layout->setView($prevView);
+
+                $this->view->prevView = $prevView;
+                $this->view->articlePreviewForm = $articlePreviewForm;
+                $this->view->article = $data;
+                $this->render('preview');
+            }
+            elseif (isset($data['backFromPreview'])) {
+                if ($data['isNew'] == 1) {
+                    $this->forward('create');
+                }
+                else {
+                    $this->forward('edit');
+                }
+            }
+        }
     }
 
     /**
---
+++ application/modules/admin/forms/ArticlePreview.php
@@ -0,0 +1,11 @@
+<?php
+
+class Admin_Form_ArticlePreview extends Zend_Form {
+
+    protected $_iniFile = '/modules/admin/forms/ArticlePreview.ini';
+
+
+    public function init() {
+        $this->setConfig(new Zend_Config_Ini(APPLICATION_PATH . $this->_iniFile));
+    }
+}
