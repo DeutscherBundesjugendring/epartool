c95106d786610cdc2cf79fb80b0abd808f1ab52b
Martin Bohal <mb@mbohal.cz>
2013-12-12 10:54:51 +0100

    DBJR-65: added required library and db patch

---
+++ library/Dbjr/NP/Db/Table/NestedSet/Exception.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * NP-DbTableNestedSet
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is
+ * bundled with this package in the file LICENSE.txt.
+ */
+
+require_once 'Zend/Db/Table/Exception.php';
+
+/**
+ * Exception in NP_Db_Table_NestedSet class.
+ *
+ * @author Nikola Posa <posa.nikola@gmail.com>
+ * @license http://opensource.org/licenses/gpl-3.0.html GNU General Public License
+ */
+class NP_Db_Table_NestedSet_Exception extends Zend_Db_Table_Exception
+{
+}
