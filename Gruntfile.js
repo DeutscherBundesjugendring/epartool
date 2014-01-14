4b500f1476890d948d0c20484c931690e0e6406f
Martin Bohal <mb@mbohal.cz>
2014-01-14 14:56:37 +0100

    DBJR-74: jquery.ui is now installed by bower and min file generated by grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,7 +35,7 @@ module.exports = function(grunt) {
 
 		// Concat all JS
 		concat: {
-			dist: {
+			bootstrap: {
 				options: {
 					separator: ';'
 				},
@@ -55,6 +55,48 @@ module.exports = function(grunt) {
 					'www/components/bootstrap/js/bootstrap-typeahead.js'
 				],
 				dest: 'www/js/bootstrap.js'
+			},
+			jqueryUi: {
+				options: {
+					separator: ';'
+				},
+				src: [
+                    'www/components/jquery.ui/ui/jquery.ui.accordion.js',
+                    'www/components/jquery.ui/ui/jquery.ui.autocomplete.js',
+                    'www/components/jquery.ui/ui/jquery.ui.button.js',
+                    'www/components/jquery.ui/ui/jquery.ui.core.js',
+                    'www/components/jquery.ui/ui/jquery.ui.datepicker.js',
+                    'www/components/jquery.ui/ui/jquery.ui.dialog.js',
+                    'www/components/jquery.ui/ui/jquery.ui.draggable.js',
+                    'www/components/jquery.ui/ui/jquery.ui.droppable.js',
+                    'www/components/jquery.ui/ui/jquery.ui.effect-blind.js',
+                    'www/components/jquery.ui/ui/jquery.ui.effect-bounce.js',
+                    'www/components/jquery.ui/ui/jquery.ui.effect-clip.js',
+                    'www/components/jquery.ui/ui/jquery.ui.effect-drop.js',
+                    'www/components/jquery.ui/ui/jquery.ui.effect-explode.js',
+                    'www/components/jquery.ui/ui/jquery.ui.effect-fade.js',
+                    'www/components/jquery.ui/ui/jquery.ui.effect-fold.js',
+                    'www/components/jquery.ui/ui/jquery.ui.effect-highlight.js',
+                    'www/components/jquery.ui/ui/jquery.ui.effect-pulsate.js',
+                    'www/components/jquery.ui/ui/jquery.ui.effect-scale.js',
+                    'www/components/jquery.ui/ui/jquery.ui.effect-shake.js',
+                    'www/components/jquery.ui/ui/jquery.ui.effect-slide.js',
+                    'www/components/jquery.ui/ui/jquery.ui.effect-transfer.js',
+                    'www/components/jquery.ui/ui/jquery.ui.effect.js',
+                    'www/components/jquery.ui/ui/jquery.ui.menu.js',
+                    'www/components/jquery.ui/ui/jquery.ui.mouse.js',
+                    'www/components/jquery.ui/ui/jquery.ui.position.js',
+                    'www/components/jquery.ui/ui/jquery.ui.progressbar.js',
+                    'www/components/jquery.ui/ui/jquery.ui.resizable.js',
+                    'www/components/jquery.ui/ui/jquery.ui.selectable.js',
+                    'www/components/jquery.ui/ui/jquery.ui.slider.js',
+                    'www/components/jquery.ui/ui/jquery.ui.sortable.js',
+                    'www/components/jquery.ui/ui/jquery.ui.spinner.js',
+                    'www/components/jquery.ui/ui/jquery.ui.tabs.js',
+                    'www/components/jquery.ui/ui/jquery.ui.tooltip.js',
+                    'www/components/jquery.ui/ui/jquery.ui.widget.js'
+				],
+				dest: 'www/js/jquery.ui.js'
 			}
 		},
 
@@ -62,14 +104,16 @@ module.exports = function(grunt) {
 		uglify: {
 			dist: {
 				files: {
-					'www/js/bootstrap.min.js': ['www/js/bootstrap.js']
+					'www/js/bootstrap.min.js': ['www/js/bootstrap.js'],
+					'www/js/jquery.ui.min.js': ['www/js/jquery.ui.js']
 				}
 			}
 		},
 
 		// Clean temporary files
 		clean: [
-			'www/js/bootstrap.js'
+			'www/js/bootstrap.js',
+			'www/js/jquery.ui.js'
 		],
 
 		// Watch task
