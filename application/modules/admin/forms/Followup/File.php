771416e580ef2ba36d6256781586af292e187a2f
Martin Bohal <mb@mbohal.cz>
2014-02-17 16:16:45 +0100

    DBJR-84: coding styles fixed

--- application/modules/admin/forms/Followup/File.php
+++ application/modules/admin/forms/Followup/File.php
@@ -25,18 +25,37 @@ class Admin_Form_Followup_File extends Zend_Form
         $this->getElement('ref_doc')->setAttrib('id', 'ref_doc');
         $this->getElement('gfx_who')->setAttrib('id', 'gfx_who');
         $formid = $this->getAttrib('id');
+        $baseUrl = Zend_Controller_Front::getInstance()->getBaseUrl();
+
         $urlparams = "?formid=$formid&elemid=".$this->getElement('ref_doc')->getAttrib('id');
-        $this->getElement('ref_doc')->setDecorators(array(
-             'Label','ViewHelper',
-                array('Popuplink',
-                        array('url'=>Zend_Controller_Front::getInstance()->getBaseUrl().'/admin/media/choose/kid/'.$kid.$urlparams, 'text' => 'Datei wählen'))
-        ));
+        $this->getElement('ref_doc')->setDecorators(
+            array(
+                'Label',
+                'ViewHelper',
+                array(
+                    'Popuplink',
+                    array(
+                        'url' => $baseUrl . '/admin/media/choose/kid/' . $kid . $urlparams,
+                        'text' => 'Datei wählen'
+                    )
+                )
+            )
+        );
+
         $urlparams = "?formid=$formid&elemid=".$this->getElement('gfx_who')->getAttrib('id');
-        $this->getElement('gfx_who')->setDecorators(array(
-             'Label','ViewHelper',
-                array('Popuplink',
-                        array('url'=>Zend_Controller_Front::getInstance()->getBaseUrl().'/admin/media/choose/kid/'.$kid.$urlparams, 'text' => 'Datei wählen'))
-        ));
+        $this->getElement('gfx_who')->setDecorators(
+            array(
+                'Label',
+                'ViewHelper',
+                array(
+                    'Popuplink',
+                    array(
+                        'url' => $baseUrl . '/admin/media/choose/kid/' . $kid . $urlparams,
+                        'text' => 'Datei wählen'
+                    )
+                )
+            )
+        );
 
     }
 }
