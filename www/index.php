20a33a6b860bf477eaca0bafb895a82af9df7cc6
Martin Bohal <mb@mbohal.cz>
2014-03-21 18:24:38 +0100

    DBJR-75: application_env to be set in top level index.php

--- www/index.php
+++ www/index.php
@@ -4,10 +4,6 @@
 defined('APPLICATION_PATH')
     || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
 
-// Define application environment
-defined('APPLICATION_ENV')
-    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
-
 // Ensure library/ is on include_path
 set_include_path(
     implode(
