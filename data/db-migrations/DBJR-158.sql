840a5f8a2ddc2a85bb54890c4ca42b33c5ccc332
Martin Bohal <mb@mbohal.cz>
2014-05-23 18:21:15 +0200

    DBJR-158: removed unused coumns from table proj

---
+++ data/db-migrations/DBJR-158.sql
@@ -0,0 +1,9 @@
+ALTER TABLE `proj`
+    DROP COLUMN `email`,
+    DROP COLUMN `realnm`,
+    DROP COLUMN `smtp_srv`,
+    DROP COLUMN `smtp_prt`,
+    DROP COLUMN `smtp_usr`,
+    DROP COLUMN `smtp_pwd`,
+    DROP COLUMN `toolline`;
+
