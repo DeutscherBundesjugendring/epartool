23765030f96077d9003d530331d662f792170172
Adam Kudrna <adam@adamkudrna.cz>
2014-06-25 18:51:50 +0200

    DBJR-143: added shortcuts to consultation navigation to enable quick adding new items

--- application/modules/admin/views/helpers/ConsultationNavigation.php
+++ application/modules/admin/views/helpers/ConsultationNavigation.php
@@ -12,78 +12,75 @@ class Admin_View_Helper_ConsultationNavigation extends Zend_View_Helper_Abstract
         $kid = $request->getParam('kid', 0);
         $html = '';
 
-        // TODO move to ini file?
         if ($kid > 0) {
             $items = array(
                 array(
                     'name' => 'dashboard',
                     'label' => 'Dashboard',
-                    'href' => $this->view->baseUrl() . '/admin/consultation/index/kid/' . $kid,
+                    'href' => $this->view->url(array('controller' => 'consultation', 'action' => 'index', 'kid' => $kid)),
                 ),
                 array(
                     'name' => 'info',
                     'label' => 'Info',
-                    'href' => $this->view->baseUrl() . '/admin/article/index/kid/' . $kid,
+                    'href' => $this->view->url(array('controller' => 'article', 'action' => 'index', 'kid' => $kid)),
+                    'new_item' => $this->view->url(array('controller' => 'article', 'action' => 'create', 'kid' => $kid)),
                 ),
                 array(
                     'name' => 'questions',
                     'label' => 'Questions',
-                    'href' => $this->view->baseUrl() . '/admin/question/index/kid/' . $kid,
+                    'href' => $this->view->url(array('controller' => 'question', 'action' => 'index', 'kid' => $kid)),
+                    'new_item' => $this->view->url(array('controller' => 'question', 'action' => 'create', 'kid' => $kid)),
                 ),
                 array(
                     'name' => 'contributions',
                     'label' => 'Contributions',
-                    'href' => $this->view->baseUrl() . '/admin/input/index/kid/' . $kid,
+                    'href' => $this->view->url(array('controller' => 'input', 'action' => 'index', 'kid' => $kid)),
                 ),
-                /*array(
-                    'name' => 'media',
-                    'label' => 'Media',
-                    'href' => $this->view->baseUrl() . '/admin/media/index/kid/' . $kid,
-                ),*/
                 array(
                     'name' => 'voting-prepare',
                     'label' => 'Voting',
-                    'href' => $this->view->baseUrl() . '/admin/votingprepare/index/kid/' . $kid,
+                    'href' => $this->view->url(array('controller' => 'votingprepare', 'action' => 'index', 'kid' => $kid)),
                 ),
                 array(
                     'name' => 'voting-settings',
                     'label' => 'Settings',
-                    'href' => $this->view->baseUrl() . '/admin/voting/settings/kid/' . $kid,
+                    'href' => $this->view->url(array('controller' => 'voting', 'action' => 'settings', 'kid' => $kid)),
                     'level' => 2,
                 ),
                 array(
                     'name' => 'voting-permissions',
                     'label' => 'Permissions',
-                    'href' => $this->view->baseUrl() . '/admin/voting/index/kid/' . $kid,
+                    'href' => $this->view->url(array('controller' => 'voting', 'action' => 'index', 'kid' => $kid)),
                     'level' => 2,
                 ),
                 array(
                     'name' => 'voting-invitations',
                     'label' => 'Invitations',
-                    'href' => $this->view->baseUrl() . '/admin/voting/invitations/kid/' . $kid,
+                    'href' => $this->view->url(array('controller' => 'voting', 'action' => 'invitations', 'kid' => $kid)),
                     'level' => 2,
                 ),
                 array(
                     'name' => 'voting-participants',
                     'label' => 'Participants',
-                    'href' => $this->view->baseUrl() . '/admin/voting/participants/kid/' . $kid,
+                    'href' => $this->view->url(array('controller' => 'voting', 'action' => 'participants', 'kid' => $kid)),
                     'level' => 2,
                 ),
                 array(
                     'name' => 'voting-results',
                     'label' => 'Results',
-                    'href' => $this->view->baseUrl() . '/admin/voting/results/kid/' . $kid,
+                    'href' => $this->view->url(array('controller' => 'voting', 'action' => 'results', 'kid' => $kid)),
                     'level' => 2,
                 ),
                 array(
                     'name' => 'followup',
                     'label' => 'Follow Up',
-                    'href' => $this->view->baseUrl() . '/admin/followup/index/kid/' . $kid,
+                    'href' => $this->view->url(array('controller' => 'followup', 'action' => 'index', 'kid' => $kid)),
+                    'new_item' => $this->view->url(array('controller' => 'followup', 'action' => 'create-file', 'kid' => $kid)),
                 ),
                 array(
                     'name' => 'stats',
                     'label' => 'Statistics',
-                    'href' => $this->view->baseUrl() . '/admin/consultation/report/kid/' . $kid,
+                    'href' => $this->view->url(array('controller' => 'consultation', 'action' => 'report', 'kid' => $kid)),
                 ),
             );
 
@@ -91,7 +88,7 @@ class Admin_View_Helper_ConsultationNavigation extends Zend_View_Helper_Abstract
 
             foreach ($items as $item) {
                 $current_user = Zend_Auth::getInstance()->getIdentity();
-                if (!array_key_exists('required_userlevel', $item) || $current_user->lvl === $item['required_userlevel']) {
+                if (!array_key_exists('required_user_level', $item) || $current_user->lvl === $item['required_user_level']) {
                     if (isset($item['level']) && $item['level'] === 2) {
                         $html .= '<a href="' . $item['href'] . '" class="list-group-item list-group-item-nested' . ($item['name'] === $activeItem ? ' active' : '' ) . '">';
                         $html .= $item['label'];
@@ -100,6 +97,9 @@ class Admin_View_Helper_ConsultationNavigation extends Zend_View_Helper_Abstract
                         $html .= '<a href="' . $item['href'] . '" class="list-group-item' . ($item['name'] === $activeItem ? ' active' : '' ) . '">';
                         $html .= '<h4 class="list-group-item-heading">' . $item['label'] . '</h4>';
                         $html .= "</a>\n";
+                    }
+                    if (isset($item['new_item'])) {
+                        $html .= '<a href="' . $item['new_item'] . '" class="list-group-add" title="Create new item"><span class="glyphicon glyphicon-plus-sign"></span></a>' . "\n";
                     }
                 }
             }
