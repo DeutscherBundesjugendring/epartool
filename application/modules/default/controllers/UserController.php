0aed7ca26d0777ccdf9f5f992a975acbc1796ada
Martin Bohal <mb@mbohal.cz>
2014-07-18 15:18:03 +0200

    DBJR-218: merged in fix from karsten with code cleanup

--- application/modules/default/controllers/UserController.php
+++ application/modules/default/controllers/UserController.php
@@ -13,6 +13,13 @@ class UserController extends Zend_Controller_Action
     {
         $this->_auth = Zend_Auth::getInstance();
         $this->_flashMessenger = $this->_helper->getHelper('flashMessenger');
+
+        $kid = $this->getRequest()->getParam('kid', 0);
+        $consultationModel = new Model_Consultations();
+        $consultation = $consultationModel->find($kid)->current();
+        if ($consultation) {
+            $this->_consultation = $consultation;
+        }
     }
 
     /**
@@ -86,7 +93,7 @@ class UserController extends Zend_Controller_Action
 
     public function inputlistAction()
     {
-        $kid = $this->_request->getParam('kid', 0);
+        $kid = isset($this->_consultation->kid) ? $this->_consultation->kid : 0;
         $consultationModel = new Model_Consultations();
         if ($this->_auth->hasIdentity()) {
             $identity = $this->_auth->getIdentity();
@@ -120,5 +127,147 @@ class UserController extends Zend_Controller_Action
             }
         }
         $this->view->form = $form;
+    }
+
+    /**
+     * List Users By Group
+     */
+    public function userlistAction()
+    {
+        $kid = isset($this->_consultation->kid) ? $this->_consultation->kid : 0;
+        $consultationList = array();
+
+        if (!$this->_auth->hasIdentity()) {
+            $this->_flashMessenger->addMessage('Bitte erst anmelden!', 'error'); // No Login
+        } else {
+            $identity = $this->_auth->getIdentity();
+            // No Consultation ID given list consultations
+            if ($kid == 0) {
+                $consultationModel = new Model_Consultations();
+                $groupsModel = new Model_Votes_Groups();
+                $consultationList = $consultationModel->getByUser($identity->uid)->toArray();
+                foreach ($consultationList as $key => $value) {
+                    $group = array();
+                    $group = $groupsModel->getByConsultation($value['kid'], $identity->uid);
+                    // if no group member in consultation delete consultation from array
+                    if (empty($group)) {
+                        unset($consultationList[$key]);
+                    }
+                }
+                $this->view->consultationList = $consultationList;
+
+            } elseif ($kid > 0) {
+                $groupsModel = new Model_Votes_Groups();
+                $consultationModel = new Model_Consultations();
+                $this->view->consultationList = $consultationList;
+                $group = $groupsModel->getByConsultation($kid, $identity->uid);
+                // list group members
+                if (count($group) > 0) {
+                    $inputModel = new Model_Inputs;
+                    $consultationModel = new Model_Consultations();
+                    $this->view->consultation = $consultationModel->find($kid)->current();
+
+                    $filter = array(array(
+                            'field'=>'vot',
+                        'operator'=>'=',
+                        'value'=>'y'
+                    ));
+                    $this->view->inputs = $inputModel->getCountByConsultationFiltered($kid, $filter);
+                    $this->view->group = $group;
+                    $this->view->identity =$identity;
+                } else {
+                    // its possible the user reload's the page
+                    $consultationList = $consultationModel->getByUser($identity->uid)->toArray();
+                    foreach ($consultationList as $key => $value) {
+                        $group = array();
+                        $group = $groupsModel->getByConsultation($value['kid'], $identity->uid);
+                        // if no group member delete consultation from array
+                        if (empty($group)) {
+                            unset($consultationList[$key]);
+                        }
+                    }
+                    // all group-member deleted ?
+                    empty($consultationList) ? $this->redirect('/user/userlist/') : $this->view->consultationList = $consultationList;
+                }
+            }
+        }
+    }
+
+    /**
+     * Ajaxresponse from userlistAction by click deny link
+     */
+    public function denyAction()
+    {
+        $this->_helper->layout()->disableLayout();
+        if (!$this->getRequest()->isXmlHttpRequest()) {
+            exit; //no AjaxRequest
+        }
+        if (!$this->_auth->hasIdentity()) {
+            exit; //no Login
+        }
+
+        $uid = $this->_request->getParam('uid', 0);
+        $sub_uid = $this->_request->getParam('subuid', 0);
+        $kid = isset($this->_consultation->kid) ? $this->_consultation->kid : 0;
+
+        $votesGroupsModel = new Model_Votes_Groups();
+        if ($votesGroupsModel->denyVoter($kid, $uid, $sub_uid)) {
+            $user = array();
+            $user['uid'] = $uid;
+            $user['sub_uid'] = $sub_uid;
+            $this->view->user = $user;
+        } else {
+            $this->view->error = 'error';
+        }
+    }
+
+    /**
+     * Ajaxresponse from userlistAction by click confirm link
+     */
+    public function confirmAction()
+    {
+        $this->_helper->layout()->disableLayout();
+        if (!$this->getRequest()->isXmlHttpRequest()) {
+            exit; //no AjaxRequest
+        }
+        if (!$this->_auth->hasIdentity()) {
+            exit; //no Login
+        }
+
+        $uid = $this->_request->getParam('uid', 0);
+        $sub_uid = $this->_request->getParam('subuid', 0);
+        $kid = isset($this->_consultation->kid) ? $this->_consultation->kid : 0;
+
+        $votesGroupsModel = new Model_Votes_Groups();
+        if ($votesGroupsModel->confirmVoter($kid, $uid, $sub_uid)) {
+            $user = array();
+            $user['uid'] = $uid;
+            $user['sub_uid'] = $sub_uid;
+            $this->view->user = $user;
+        } else {
+            $this->view->error = 'error';
+        }
+    }
+
+    /**
+     * Ajaxresponse from uuserlistAction by click delete link
+     */
+    public function deleteAction()
+    {
+        $this->_helper->layout()->disableLayout();
+        if (!$this->getRequest()->isXmlHttpRequest()) {
+            exit; //no AjaxRequest
+        }
+        if (!$this->_auth->hasIdentity()) {
+            exit; //no Login
+        }
+
+        $uid = $this->_request->getParam('uid', 0);
+        $sub_uid = $this->_request->getParam('subuid', 0);
+        $kid = isset($this->_consultation->kid) ? $this->_consultation->kid : 0;
+        $votesGroupsModel = new Model_Votes_Groups();
+        if ($votesGroupsModel->deleteVoter($kid, $uid, $sub_uid) == 0) {
+            $this->view->error = 'error';
+        }
     }
 }
