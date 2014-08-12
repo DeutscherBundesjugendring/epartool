97dcf6a336453d7312ed63038704b6b4b0191a87
Martin Bohal <mb@mbohal.cz>
2014-08-12 16:39:47 +0200

    DBJR-179: admin consultation navigation supports marking sections as active even for nested menus

--- application/modules/admin/views/helpers/ConsultationNavigation.php
+++ application/modules/admin/views/helpers/ConsultationNavigation.php
@@ -42,36 +42,33 @@ class Admin_View_Helper_ConsultationNavigation extends Zend_View_Helper_Abstract
                     'name' => 'voting-prepare',
                     'label' => 'Voting',
                     'href' => $this->view->url(array('controller' => 'votingprepare', 'action' => 'index', 'kid' => $kid)),
-                ),
-                array(
-                    'name' => 'voting-settings',
-                    'label' => 'Settings',
-                    'href' => $this->view->url(array('controller' => 'voting', 'action' => 'settings', 'kid' => $kid)),
-                    'level' => 2,
-                ),
-                array(
-                    'name' => 'voting-permissions',
-                    'label' => 'Permissions',
-                    'href' => $this->view->url(array('controller' => 'voting', 'action' => 'index', 'kid' => $kid)),
-                    'level' => 2,
-                ),
-                array(
-                    'name' => 'voting-invitations',
-                    'label' => 'Invitations',
-                    'href' => $this->view->url(array('controller' => 'voting', 'action' => 'invitations', 'kid' => $kid)),
-                    'level' => 2,
-                ),
-                array(
-                    'name' => 'voting-participants',
-                    'label' => 'Participants',
-                    'href' => $this->view->url(array('controller' => 'voting', 'action' => 'participants', 'kid' => $kid)),
-                    'level' => 2,
-                ),
-                array(
-                    'name' => 'voting-results',
-                    'label' => 'Results',
-                    'href' => $this->view->url(array('controller' => 'voting', 'action' => 'results', 'kid' => $kid)),
-                    'level' => 2,
+                    'children' => [
+                        array(
+                            'name' => 'voting-settings',
+                            'label' => 'Settings',
+                            'href' => $this->view->url(array('controller' => 'voting', 'action' => 'settings', 'kid' => $kid)),
+                        ),
+                        array(
+                            'name' => 'voting-permissions',
+                            'label' => 'Permissions',
+                            'href' => $this->view->url(array('controller' => 'voting', 'action' => 'index', 'kid' => $kid)),
+                        ),
+                        array(
+                            'name' => 'voting-invitations',
+                            'label' => 'Invitations',
+                            'href' => $this->view->url(array('controller' => 'voting', 'action' => 'invitations', 'kid' => $kid)),
+                        ),
+                        array(
+                            'name' => 'voting-participants',
+                            'label' => 'Participants',
+                            'href' => $this->view->url(array('controller' => 'voting', 'action' => 'participants', 'kid' => $kid)),
+                        ),
+                        array(
+                            'name' => 'voting-results',
+                            'label' => 'Results',
+                            'href' => $this->view->url(array('controller' => 'voting', 'action' => 'results', 'kid' => $kid)),
+                        ),
+                    ]
                 ),
                 array(
                     'name' => 'followup',
@@ -85,10 +82,12 @@ class Admin_View_Helper_ConsultationNavigation extends Zend_View_Helper_Abstract
                     'href' => $this->view->url(array('controller' => 'consultation', 'action' => 'report', 'kid' => $kid)),
                 ),
                  array(
+                    'name' => '',
                     'label' => 'Beteiligungsrunde schließen',
                     'href' => $this->view->baseUrl() . '/admin/close/index/kid/' . $kid
                 ),
                 array(
+                    'name' => '',
                     'label' => 'Beteiligungsrunde löschen',
                     'href' => $this->view->baseUrl() . '/admin/consultation/delete/kid/' . $kid,
                     'class' => 'button_red delete-action',
@@ -96,27 +95,43 @@ class Admin_View_Helper_ConsultationNavigation extends Zend_View_Helper_Abstract
                 ),
             );
 
-            $html .= '<div class="list-group list-group-nested">' . "\n";
+            $renderItemFnc = function (&$item, &$html, $isNested = null) use ($activeItem, &$renderItemFnc)
+            {
 
+                $childrenHtml = '';
+                if (isset($item['children'])) {
+                    foreach ($item['children'] as $child) {
+                        $renderItemFnc($child, $childrenHtml, true);
+                        $item['active'] = !empty($item['active']) ? true : $child['active'];
+                    }
+                }
+
+                $item['active'] = !empty($item['active']) ? true : ($item['name'] === $activeItem);
+                if ($isNested) {
+                    $html .= '<a href="' . $item['href'] . '" class="list-group-item list-group-item-nested' . ($item['active'] ? ' active' : '' ) . '">';
+                    $html .= $item['label'];
+                    $html .= "</a>";
+                } else {
+                    $html .= '<a href="' . $item['href'] . '" class="list-group-item' . ($item['active'] ? ' active' : '' ) . '">';
+                    $html .= '<h4 class="list-group-item-heading">' . $item['label'] . '</h4>';
+                    $html .= "</a>";
+                }
+                $html .= $childrenHtml;
+
+            };
+
+            $html .= '<div class="list-group list-group-nested">';
             foreach ($items as $item) {
                 $current_user = Zend_Auth::getInstance()->getIdentity();
                 if (!array_key_exists('required_user_level', $item) || $current_user->lvl === $item['required_user_level']) {
-                    if (isset($item['level']) && $item['level'] === 2) {
-                        $html .= '<a href="' . $item['href'] . '" class="list-group-item list-group-item-nested' . ($item['name'] === $activeItem ? ' active' : '' ) . '">';
-                        $html .= $item['label'];
-                        $html .= "</a>\n";
-                    } else {
-                        $html .= '<a href="' . $item['href'] . '" class="list-group-item' . ($item['name'] === $activeItem ? ' active' : '' ) . '">';
-                        $html .= '<h4 class="list-group-item-heading">' . $item['label'] . '</h4>';
-                        $html .= "</a>\n";
-                    }
+                    $renderItemFnc($item, $html);
                     if (isset($item['new_item'])) {
                         $html .= '<a href="' . $item['new_item'] . '" class="list-group-add" title="Create new item"><span class="glyphicon glyphicon-plus-sign"></span></a>' . "\n";
                     }
                 }
             }
 
-            $html .= "</div>\n";
+            $html .= "</div>";
         }
 
         return $html;
