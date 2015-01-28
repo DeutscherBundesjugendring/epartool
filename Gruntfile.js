9614090ac1e8dd7d17c323bc8397740a109b6fff
Adam Kudrna <adam@adamkudrna.cz>
2015-01-29 00:55:29 +0100

    DBJR-90: Gruntfile refactoring: configurable paths, build subtasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,13 +8,82 @@ module.exports = function (grunt) {
     // Load meta info from package.json
     pkg: grunt.file.readJSON('package.json'),
 
+    paths: {
+      src: 'www/src',
+      dist: 'www',
+      bower: 'www/components/bower',
+      temp: '.tmp'
+    },
+
+    // Compile LESS
+    less: {
+      dev: {
+        options: {
+          paths: ['<%= paths.bower %>']
+        },
+        files: {
+          '<%= paths.dist %>/css/<%= pkg.name %>.css': '<%= paths.src %>/less/main.less',
+          '<%= paths.dist %>/css/admin.css': '<%= paths.src %>/less/admin.less',
+          '<%= paths.temp %>/mail.css': '<%= paths.src %>/less/mail.less'
+        }
+      },
+      dist: {
+        options: {
+          paths: ['<%= paths.bower %>'],
+          yuicompress: true
+        },
+        files: {
+          '<%= paths.dist %>/css/bootstrap.min.css': '<%= paths.bower %>/bootstrap/less/bootstrap.less',
+          '<%= paths.dist %>/css/<%= pkg.name %>.min.css': '<%= paths.src %>/less/main.less'
+        }
+      }
+    },
+
+    // Remove unused CSS
+    uncss: {
+      mail: {
+        src: ['application/layouts/scripts/src/*.phtml'],
+        dest: '<%= paths.temp %>/mail_clean.css',
+        options: {
+          report: 'min' // optional: include to report savings
+        }
+      }
+    },
+
+    // Process HTML
+    processhtml: {
+      mail: {
+        files: {
+          '<%= paths.temp %>/mail.phtml': ['application/layouts/scripts/src/mail.phtml']
+        }
+      }
+    },
+
+    // Inject inline CSS to mail templates from linked stylesheets.
+    // Behold! Requires Premailer gem installed in the system (gem install premailer).
+    premailer: {
+      main: {
+        options: {
+          verbose: true
+        },
+        files: {
+          'application/layouts/scripts/mail.phtml': ['<%= paths.temp %>/mail.phtml']
+        }
+      }
+    },
+
+    // Lint custom JS
+    jshint: {
+      files: ['<%= paths.src %>/js/main.js']
+    },
+
     // Compile Coffee script
     coffee: {
       compile: {
         files: {
-          "www/js/admin.js": "www/coffee/admin.coffee",
-          "www/js/web.js": "www/coffee/web.coffee",
-          "www/js/admin_mediaPopup.js": "www/coffee/admin_mediaPopup.coffee"
+          '<%= paths.dist %>/js/admin.js': '<%= paths.src %>/coffee/admin.coffee',
+          '<%= paths.dist %>/js/web.js': '<%= paths.src %>/coffee/web.coffee',
+          '<%= paths.dist %>/js/admin_mediaPopup.js': '<%= paths.src %>/coffee/admin_mediaPopup.coffee'
         }
       }
     },
@@ -28,38 +97,13 @@ module.exports = function (grunt) {
           }
         },
         files: {
-          'www/js/i18n/admin.cs.js': ['languages/cs/admin-js.po'],
-          'www/js/i18n/admin.en.js': ['languages/en/admin-js.po'],
-          'www/js/i18n/admin.de.js': ['languages/de/admin-js.po'],
-          'www/js/i18n/admin.es.js': ['languages/es/admin-js.po'],
-          'www/js/i18n/admin.fr.js': ['languages/fr/admin-js.po']
+          '<%= paths.dist %>/js/i18n/admin.cs.js': ['languages/cs/admin-js.po'],
+          '<%= paths.dist %>/js/i18n/admin.en.js': ['languages/en/admin-js.po'],
+          '<%= paths.dist %>/js/i18n/admin.de.js': ['languages/de/admin-js.po'],
+          '<%= paths.dist %>/js/i18n/admin.es.js': ['languages/es/admin-js.po'],
+          '<%= paths.dist %>/js/i18n/admin.fr.js': ['languages/fr/admin-js.po']
         }
       }
-    },
-
-    // Compile LESS
-    less: {
-      dev: {
-        files: {
-          'www/css/<%= pkg.name %>.css': 'www/less/main.less',
-          'www/css/admin.css': 'www/less/admin.less',
-          '.tmp/mail.css': 'www/less/mail.less'
-        }
-      },
-      dist: {
-        options: {
-          yuicompress: true
-        },
-        files: {
-          'www/css/bootstrap.min.css': 'www/components/bower/bootstrap/less/bootstrap.less',
-          'www/css/<%= pkg.name %>.min.css': 'www/less/main.less'
-        }
-      }
-    },
-
-    // Lint custom JS
-    jshint: {
-      files: ['www/js/main.js']
     },
 
     // Concat all JS
@@ -69,67 +113,67 @@ module.exports = function (grunt) {
           separator: ';'
         },
         src: [
-          'www/components/bower/bootstrap/js/bootstrap-affix.js',
-          'www/components/bower/bootstrap/js/bootstrap-alert.js',
-          'www/components/bower/bootstrap/js/bootstrap-button.js',
-          'www/components/bower/bootstrap/js/bootstrap-carousel.js',
-          'www/components/bower/bootstrap/js/bootstrap-collapse.js',
-          'www/components/bower/bootstrap/js/bootstrap-dropdown.js',
-          'www/components/bower/bootstrap/js/bootstrap-modal.js',
-          'www/components/bower/bootstrap/js/bootstrap-scrollspy.js',
-          'www/components/bower/bootstrap/js/bootstrap-tab.js',
-          'www/components/bower/bootstrap/js/bootstrap-tooltip.js',
-          'www/components/bower/bootstrap/js/bootstrap-popover.js',
-          'www/components/bower/bootstrap/js/bootstrap-transition.js',
-          'www/components/bower/bootstrap/js/bootstrap-typeahead.js'
+          '<%= paths.bower %>/bootstrap/js/bootstrap-affix.js',
+          '<%= paths.bower %>/bootstrap/js/bootstrap-alert.js',
+          '<%= paths.bower %>/bootstrap/js/bootstrap-button.js',
+          '<%= paths.bower %>/bootstrap/js/bootstrap-carousel.js',
+          '<%= paths.bower %>/bootstrap/js/bootstrap-collapse.js',
+          '<%= paths.bower %>/bootstrap/js/bootstrap-dropdown.js',
+          '<%= paths.bower %>/bootstrap/js/bootstrap-modal.js',
+          '<%= paths.bower %>/bootstrap/js/bootstrap-scrollspy.js',
+          '<%= paths.bower %>/bootstrap/js/bootstrap-tab.js',
+          '<%= paths.bower %>/bootstrap/js/bootstrap-tooltip.js',
+          '<%= paths.bower %>/bootstrap/js/bootstrap-popover.js',
+          '<%= paths.bower %>/bootstrap/js/bootstrap-transition.js',
+          '<%= paths.bower %>/bootstrap/js/bootstrap-typeahead.js'
         ],
-        dest: 'www/js/bootstrap.js'
+        dest: '<%= paths.dist %>/js/bootstrap.js'
       },
       jqueryUi: {
         options: {
           separator: ';'
         },
         src: [
-          'www/components/bower/jquery.ui/ui/jquery.ui.core.js',
-          'www/components/bower/jquery.ui/ui/jquery.ui.widget.js',
+          '<%= paths.bower %>/jquery.ui/ui/jquery.ui.core.js',
+          '<%= paths.bower %>/jquery.ui/ui/jquery.ui.widget.js',
 
-          'www/components/bower/jquery.ui/ui/jquery.ui.position.js',
-          'www/components/bower/jquery.ui/ui/jquery.ui.mouse.js',
+          '<%= paths.bower %>/jquery.ui/ui/jquery.ui.position.js',
+          '<%= paths.bower %>/jquery.ui/ui/jquery.ui.mouse.js',
 
-          'www/components/bower/jquery.ui/ui/jquery.ui.draggable.js',
-          'www/components/bower/jquery.ui/ui/jquery.ui.droppable.js',
+          '<%= paths.bower %>/jquery.ui/ui/jquery.ui.draggable.js',
+          '<%= paths.bower %>/jquery.ui/ui/jquery.ui.droppable.js',
 
-          'www/components/bower/jquery.ui/ui/jquery.ui.accordion.js',
-          'www/components/bower/jquery.ui/ui/jquery.ui.autocomplete.js',
-          'www/components/bower/jquery.ui/ui/jquery.ui.button.js',
-          'www/components/bower/jquery.ui/ui/jquery.ui.datepicker.js',
-          'www/components/bower/jquery.ui/ui/jquery.ui.dialog.js',
-          'www/components/bower/jquery.ui/ui/jquery.ui.menu.js',
-          'www/components/bower/jquery.ui/ui/jquery.ui.progressbar.js',
-          'www/components/bower/jquery.ui/ui/jquery.ui.resizable.js',
-          'www/components/bower/jquery.ui/ui/jquery.ui.selectable.js',
-          'www/components/bower/jquery.ui/ui/jquery.ui.slider.js',
-          'www/components/bower/jquery.ui/ui/jquery.ui.sortable.js',
-          'www/components/bower/jquery.ui/ui/jquery.ui.spinner.js',
-          'www/components/bower/jquery.ui/ui/jquery.ui.tabs.js',
-          'www/components/bower/jquery.ui/ui/jquery.ui.tooltip.js',
+          '<%= paths.bower %>/jquery.ui/ui/jquery.ui.accordion.js',
+          '<%= paths.bower %>/jquery.ui/ui/jquery.ui.autocomplete.js',
+          '<%= paths.bower %>/jquery.ui/ui/jquery.ui.button.js',
+          '<%= paths.bower %>/jquery.ui/ui/jquery.ui.datepicker.js',
+          '<%= paths.bower %>/jquery.ui/ui/jquery.ui.dialog.js',
+          '<%= paths.bower %>/jquery.ui/ui/jquery.ui.menu.js',
+          '<%= paths.bower %>/jquery.ui/ui/jquery.ui.progressbar.js',
+          '<%= paths.bower %>/jquery.ui/ui/jquery.ui.resizable.js',
+          '<%= paths.bower %>/jquery.ui/ui/jquery.ui.selectable.js',
+          '<%= paths.bower %>/jquery.ui/ui/jquery.ui.slider.js',
+          '<%= paths.bower %>/jquery.ui/ui/jquery.ui.sortable.js',
+          '<%= paths.bower %>/jquery.ui/ui/jquery.ui.spinner.js',
+          '<%= paths.bower %>/jquery.ui/ui/jquery.ui.tabs.js',
+          '<%= paths.bower %>/jquery.ui/ui/jquery.ui.tooltip.js',
 
-          'www/components/bower/jquery.ui/ui/jquery.ui.effect.js',
-          'www/components/bower/jquery.ui/ui/jquery.ui.effect-blind.js',
-          'www/components/bower/jquery.ui/ui/jquery.ui.effect-bounce.js',
-          'www/components/bower/jquery.ui/ui/jquery.ui.effect-clip.js',
-          'www/components/bower/jquery.ui/ui/jquery.ui.effect-drop.js',
-          'www/components/bower/jquery.ui/ui/jquery.ui.effect-explode.js',
-          'www/components/bower/jquery.ui/ui/jquery.ui.effect-fade.js',
-          'www/components/bower/jquery.ui/ui/jquery.ui.effect-fold.js',
-          'www/components/bower/jquery.ui/ui/jquery.ui.effect-highlight.js',
-          'www/components/bower/jquery.ui/ui/jquery.ui.effect-pulsate.js',
-          'www/components/bower/jquery.ui/ui/jquery.ui.effect-scale.js',
-          'www/components/bower/jquery.ui/ui/jquery.ui.effect-shake.js',
-          'www/components/bower/jquery.ui/ui/jquery.ui.effect-slide.js',
-          'www/components/bower/jquery.ui/ui/jquery.ui.effect-transfer.js'
+          '<%= paths.bower %>/jquery.ui/ui/jquery.ui.effect.js',
+          '<%= paths.bower %>/jquery.ui/ui/jquery.ui.effect-blind.js',
+          '<%= paths.bower %>/jquery.ui/ui/jquery.ui.effect-bounce.js',
+          '<%= paths.bower %>/jquery.ui/ui/jquery.ui.effect-clip.js',
+          '<%= paths.bower %>/jquery.ui/ui/jquery.ui.effect-drop.js',
+          '<%= paths.bower %>/jquery.ui/ui/jquery.ui.effect-explode.js',
+          '<%= paths.bower %>/jquery.ui/ui/jquery.ui.effect-fade.js',
+          '<%= paths.bower %>/jquery.ui/ui/jquery.ui.effect-fold.js',
+          '<%= paths.bower %>/jquery.ui/ui/jquery.ui.effect-highlight.js',
+          '<%= paths.bower %>/jquery.ui/ui/jquery.ui.effect-pulsate.js',
+          '<%= paths.bower %>/jquery.ui/ui/jquery.ui.effect-scale.js',
+          '<%= paths.bower %>/jquery.ui/ui/jquery.ui.effect-shake.js',
+          '<%= paths.bower %>/jquery.ui/ui/jquery.ui.effect-slide.js',
+          '<%= paths.bower %>/jquery.ui/ui/jquery.ui.effect-transfer.js'
         ],
-        dest: 'www/js/jquery.ui.js'
+        dest: '<%= paths.dist %>/js/jquery.ui.js'
       }
     },
 
@@ -137,65 +181,32 @@ module.exports = function (grunt) {
     uglify: {
       dist: {
         files: {
-          'www/js/bootstrap.min.js': ['www/js/bootstrap.js'],
-          'www/js/jquery.ui.min.js': ['www/js/jquery.ui.js']
-        }
-      }
-    },
-
-    // Remove unused CSS
-    uncss: {
-      mail: {
-        src: ['application/layouts/scripts/src/*.phtml'],
-        dest: '.tmp/mail_clean.css',
-        options: {
-          report: 'min' // optional: include to report savings
-        }
-      }
-    },
-
-    // Process HTML
-    processhtml: {
-      mail: {
-        files: {
-          '.tmp/mail.phtml': ['application/layouts/scripts/src/mail.phtml']
-        }
-      }
-    },
-
-    // Inject inline CSS to mail templates from linked stylesheets.
-    // Behold! Requires Premailer gem installed in the system (gem install premailer).
-    premailer: {
-      main: {
-        options: {
-          verbose: true
-        },
-        files: {
-          'application/layouts/scripts/mail.phtml': ['.tmp/mail.phtml']
+          '<%= paths.dist %>/js/bootstrap.min.js': ['<%= paths.dist %>/js/bootstrap.js'],
+          '<%= paths.dist %>/js/jquery.ui.min.js': ['<%= paths.dist %>/js/jquery.ui.js']
         }
       }
     },
 
     // Clean temporary files
-    clean: [
-      'www/js/bootstrap.js',
-      'www/js/jquery.ui.js',
-      '.tmp'
-    ],
+    clean: {
+      css: '<%= paths.dist %>/css/*',
+      js: '<%= paths.dist %>/js/*',
+      //images: '<%= paths.dist %>/images/*',
+      temp: '<%= paths.temp %>'
+    },
 
     // Watch task
     watch: {
       less: {
-        files: ['www/less/**/*.less'],
-        tasks: ['less']
-      },
-      coffee: {
-        files: ['www/coffee/**/*.coffee'],
-        tasks: ['coffee']
+        files: ['<%= paths.src %>/less/**/*.less'],
+        tasks: ['build-css']
       },
       js: {
-        files: ['www/js/**/*(!.min).js'],
-        tasks: ['jshint']
+        files: [
+          '<%= paths.src %>/coffee/*.coffee',
+          '<%= paths.dist %>/js/**/*(!.min).js'
+        ],
+        tasks: ['build-js']
       }
     },
 
@@ -204,11 +215,11 @@ module.exports = function (grunt) {
       dev: {
         bsFiles: {
           src: [
-            'www/css/*.css',
-            'www/js/*.js',
-            'www/images/**/*.svg',
-            'www/images/**/*.png',
-            'www/images/**/*.jpg',
+            '<%= paths.dist %>/css/*.css',
+            '<%= paths.dist %>/js/*.js',
+            '<%= paths.dist %>/images/**/*.svg',
+            '<%= paths.dist %>/images/**/*.png',
+            '<%= paths.dist %>/images/**/*.jpg',
             'application/layouts/scripts/*.phtml',
             'application/modules/admin/views/**/*.phtml',
             'application/modules/default/views/**/*.phtml'
@@ -232,22 +243,35 @@ module.exports = function (grunt) {
     'clean'
   ]);
 
-  // Default task
-  grunt.registerTask('build', [
-    'po2json',
-    'less',
-    'coffee',
-    'jshint',
-    'concat',
-    'uglify',
-    'clean'
+  // Build subtasks
+  grunt.registerTask('build-css', [
+    'clean:css',
+    'less'
   ]);
 
+  grunt.registerTask('build-js', [
+    'clean:js',
+    'jshint',
+    'coffee',
+    'concat',
+    'uglify',
+    'po2json'
+  ]);
+
+  // Build task
+  grunt.registerTask('build', [
+    'clean:temp',
+    'build-css',
+    'build-js'
+  ]);
+
+  // Development
   grunt.registerTask('dev', [
     'build',
     'browserSync',
     'watch'
   ]);
 
+  // Default task
   grunt.registerTask('default', 'build');
 };
