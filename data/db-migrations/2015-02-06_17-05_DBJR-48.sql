0ff9b43538089ddce966507dd375bcd604fab762
Martin Bohal <mb@mbohal.cz>
2015-02-09 00:17:10 +0100

    Fix db migration query to run automatically

--- data/db-migrations/2015-02-06_17-05_DBJR-48.sql
+++ data/db-migrations/2015-02-06_17-05_DBJR-48.sql
@@ -8,26 +8,17 @@ VALUES
 
 
 
--- The following queries has to be run for each project in database
--- SET @project_code = 'sd';
 SET @project_code = ;
 INSERT INTO `email_template_has_email_placeholder` (`email_template_id`, `email_placeholder_id`)
-VALUES
-    (
-        (
-            SELECT `id`
-            FROM `email_template`
-            WHERE `name`='input_discussion_contrib_confirmation_new_user' AND `project_code`=@project_code
-            COLLATE utf8_unicode_ci
-        ),
-        (SELECT `id` FROM `email_placeholder` WHERE `name`='video_url')
-    ),
-    (
-        (
-            SELECT `id`
-            FROM `email_template`
-            WHERE `name`='input_discussion_contrib_confirmation' AND `project_code`=@project_code
-            COLLATE utf8_unicode_ci
-        ),
-        (SELECT `id` FROM `email_placeholder` WHERE `name`='video_url')
-    );
+SELECT
+    `id`,
+    (SELECT `id` FROM `email_placeholder` WHERE `name`='video_url')
+FROM `email_template`
+WHERE `name`='input_discussion_contrib_confirmation_new_user';
+
+INSERT INTO `email_template_has_email_placeholder` (`email_template_id`, `email_placeholder_id`)
+SELECT
+    `id`,
+    (SELECT `id` FROM `email_placeholder` WHERE `name`='video_url')
+FROM `email_template`
+WHERE `name`='input_discussion_contrib_confirmation';
