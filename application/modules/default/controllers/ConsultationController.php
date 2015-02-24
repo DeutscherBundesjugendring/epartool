073e9a9ef6acb5694ccafa038a192c7fbfb64643
Adam Kudrna <adam@adamkudrna.cz>
2015-02-24 16:25:19 +0100

    DBJR-90 DBJR-441: added back the overview page with all consultations (despite they can be listed on homepage)

---
+++ application/modules/default/controllers/ConsultationController.php
@@ -0,0 +1,18 @@
+<?php
+
+class ConsultationController extends Zend_Controller_Action
+{
+    public function indexAction()
+    {
+        $consModel = new Model_Consultations();
+
+        $consultations = $consModel->fetchAll(
+            $consModel
+                ->select()
+                ->where('public=?', 'y')
+                ->order('ord DESC')
+        );
+
+        $this->view->consultations = $consultations;
+    }
+}
