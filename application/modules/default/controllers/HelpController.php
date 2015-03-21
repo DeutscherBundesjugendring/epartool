5fafd5e46d118bd7ee92951523a48cb690052747
Martin Bohal <mb@mbohal.cz>
2015-03-21 11:59:05 +0100

    DBJR-497: Do not remove tags from help texts on frontend

--- application/modules/default/controllers/HelpController.php
+++ application/modules/default/controllers/HelpController.php
@@ -13,6 +13,13 @@ class HelpController extends Zend_Controller_Action
                 ->from($helpTextModel->info(Model_HelpText::NAME), ['body', 'name'])
         );
 
+        $dom = new DOMDocument();
+        $dom->loadHTML($helpText['body'], LIBXML_HTML_NOIMPLIED + LIBXML_HTML_NODEFDTD);
+        foreach ($dom->getElementsByTagName('script') as $item) {
+          $item->parentNode->removeChild($item);
+        }
+        $helpText['body'] = $dom->saveHTML();
+
         $this->view->helpText = $helpText;
     }
 }
