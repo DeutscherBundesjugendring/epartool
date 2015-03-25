c51201ea12f82e28e1a2d041b7f1884be81ca41c
Martin Bohal <mb@mbohal.cz>
2015-03-25 16:11:16 +0100

    DBJR-364: Fix csrf token name in form

--- application/modules/admin/forms/Settings.php
+++ application/modules/admin/forms/Settings.php
@@ -89,7 +89,7 @@ class Admin_Form_Settings extends Dbjr_Form_Admin
         );
 
         // CSRF Protection
-        $hash = $this->createElement('hash', 'csrf_token_question', array('salt' => 'unique'));
+        $hash = $this->createElement('hash', 'csrf_token_settings', array('salt' => 'unique'));
         $hash->setSalt(md5(mt_rand(1, 100000) . time()));
         if (is_numeric((Zend_Registry::get('systemconfig')->adminform->general->csfr_protect->ttl))) {
             $hash->setTimeout(Zend_Registry::get('systemconfig')->adminform->general->csfr_protect->ttl);
