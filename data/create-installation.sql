b0dc16d8bb856bb99d2630095d92463a1e457208
Martin Bohal <mb@mbohal.cz>
2015-06-11 17:21:37 +0200

    Fix db installation script

--- data/create-installation.sql
+++ data/create-installation.sql
@@ -961,3 +961,93 @@ CREATE TABLE `vt_settings` (
   PRIMARY KEY (`kid`)
 ) ENGINE=InnoDB;
 
+
+
+
+
+
+
+
+-- MIGRATIONS
+
+ALTER TABLE `articles` ADD COLUMN `time_modified` timestamp NOT NULL DEFAULT '0000-00-00';
+ALTER TABLE `quests` ADD COLUMN `time_modified` timestamp NOT NULL DEFAULT '0000-00-00';
+
+
+DROP TABLE IF EXISTS `email_attachment`;
+CREATE TABLE `email_attachment` (
+    `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+    `email_id` int UNSIGNED NOT NULL,
+    `filepath` varchar(255) NOT NULL,
+    PRIMARy KEY (`id`),
+    CONSTRAINT `email_attachment_email_id_ibfk` FOREIGN KEY (`email_id`) REFERENCES `email` (`id`)
+);
+
+
+ALTER TABLE `users` CHANGE `newsl_subscr` `newsl_subscr` enum('y','n') NOT NULL DEFAULT 'n' COMMENT 'Subscription of newsletter';
+UPDATE `users` SET `newsl_subscr` = 'n' WHERE `newsl_subscr` != 'y';
+
+
+ALTER TABLE `cnslt`
+    ADD COLUMN `phase_info` varchar(50) NULL DEFAULT NULL,
+    ADD COLUMN `phase_support` varchar(50) NULL DEFAULT NULL,
+    ADD COLUMN `phase_input` varchar(50) NULL DEFAULT NULL,
+    ADD COLUMN `phase_voting` varchar(50) NULL DEFAULT NULL,
+    ADD COLUMN `phase_followup` varchar(50) NULL DEFAULT NULL;
+
+
+ALTER TABLE `tgs` DROP COLUMN `tg_en`;
+
+
+ALTER TABLE `dirs`
+    DROP COLUMN `left`,
+    DROP COLUMN `right`,
+    ADD COLUMN `order` smallint unsigned NULL DEFAULT NULL;
+
+
+DROP TABLE `tgs_used`;
+
+DROP TABLE `discussns`;
+DROP TABLE `discuss`;
+
+CREATE TABLE `input_discussion` (
+    `id` int unsigned NOT NULL AUTO_INCREMENT,
+    `input_id` int unsigned NOT NULL,
+    `user_id` int unsigned NOT NULL,
+    `time_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `is_visible` boolean DEFAULT FALSE NOT NULL,
+    `is_user_confirmed` boolean DEFAULT FALSE NOT NULL,
+    `body` text NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `input_discussion_time_created_idx` (`time_created`),
+    INDEX `input_discussion_is_visible_idx` (`is_visible`),
+    CONSTRAINT `input_discussion_input_id_fkey` FOREIGN KEY (`input_id`) REFERENCES `inpt` (`tid`),
+    CONSTRAINT `input_discussion_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users` (`uid`)
+) ENGINE=InnoDb, COLLATE=utf8_unicode_ci;
+
+ALTER TABLE `inpt`
+    ADD COLUMN `input_discussion_contrib` int unsigned NULL DEFAULT NULL,
+    ADD CONSTRAINT `input_discussion_contrib_fkey` FOREIGN KEY (`input_discussion_contrib`) REFERENCES `input_discussion` (`id`);
+
+INSERT INTO `email_placeholder` (`name`, `description`, `is_global`)
+VALUES
+    ('contribution_text', 'The text of the contribution.', 0),
+    ('input_thes', 'The theses part of the input.', 0),
+    ('input_expl', 'The explanation part of the input.', 0);
+
+
+INSERT INTO `notification_type` (`name`)
+VALUES
+    ('input_discussion_contribution_created');
+
+ALTER TABLE `email_template` CHANGE `name` `name` varchar(100) NOT NULL;
+
+UPDATE `email_template` SET `name` = 'question_subscription_confirmation' WHERE `name` = 'subscription_confirmation';
+UPDATE `email_template` SET `name` = 'question_subscription_confirmation_new_user' WHERE `name` = 'subscription_confirmation_new_user';
+
+
+ALTER TABLE `cnslt`
+    CHANGE `img_file` `img_file` varchar(64) NULL DEFAULT NULL COMMENT 'File name of the title image.';
+
+
+ALTER TABLE `cnslt` DROP COLUMN `summ_show`;
