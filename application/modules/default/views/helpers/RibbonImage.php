e847b6285f516bf95c2813b9855d6d8a4f411205
Adam Kudrna <adam@adamkudrna.cz>
2015-06-21 18:18:14 +0200

    DBJR-481: refactor stickers from static images to translatable content

--- application/modules/default/views/helpers/RibbonImage.php
+++ application/modules/default/views/helpers/RibbonImage.php
@@ -13,41 +13,34 @@ class Module_Default_View_Helper_RibbonImage extends Zend_View_Helper_Abstract
         $nowDate = Zend_Date::now();
 
         if ($nowDatePlusOne->isEarlier(new Zend_Date($con->inp_to, Zend_Date::ISO_8601))) {
-            $imgPart = 'mitmachen';
             $text = $this->view->translate('Participate');
         } elseif ($nowDate->isEarlier(new Zend_Date($con->inp_to, Zend_Date::ISO_8601))
             && $nowDatePlusOne->isLater(new Zend_Date($con->inp_to, Zend_Date::ISO_8601))
         ) {
-            $imgPart = 'nur-noch-heute';
             $text = $this->view->translate('only today');
         } elseif ($nowDate->isLater(new Zend_Date($con->inp_to, Zend_Date::ISO_8601))
             && $nowDate->isEarlier(new Zend_Date($con->vot_fr, Zend_Date::ISO_8601))
         ) {
-            $imgPart = 'bald-abstimmen';
             $text = $this->view->translate('Voting coming up');
         } elseif ($nowDate->isLater(new Zend_Date($con->vot_fr, Zend_Date::ISO_8601))
             && $nowDatePlusOne->isEarlier(new Zend_Date($con->vot_to, Zend_Date::ISO_8601))
         ) {
-            $imgPart = 'mitmachen';
             $text = $this->view->translate('Participate');
         } elseif ($nowDate->isEarlier(new Zend_Date($con->vot_to, Zend_Date::ISO_8601))
             && $nowDatePlusOne->isLater(new Zend_Date($con->vot_to, Zend_Date::ISO_8601))
         ) {
-            $imgPart = 'nur-noch-heute';
             $text = $this->view->translate('only today');
         } elseif ($nowDate->isLater(new Zend_Date($con->vot_to, Zend_Date::ISO_8601))
             && $con->follup_show == 'n'
         ) {
-            $imgPart = 'ergebnisse';
             $text = $this->view->translate('Results');
         } elseif ($nowDate->isLater(new Zend_Date($con->vot_to, Zend_Date::ISO_8601))
             && $con->follup_show == 'y'
         ) {
-            $imgPart = 'reaktion';
             $text = $this->view->translate('Reaction');
         }
 
-        $html = '<span class="label sticker label-' . $imgPart . ' hidden-print">' . $text . '</span>';
+        $html = '<div class="sticker hidden-print"><div class="sticker-label">' . $text . '</div></div>';
 
         return $html;
     }
