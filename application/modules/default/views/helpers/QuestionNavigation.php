62b2a9762cf4b723bcd4c64d2ebe81fad64dd0dc
Adam Kudrna <adam@adamkudrna.cz>
2015-07-01 18:42:42 +0200

    Minor fixes

--- application/modules/default/views/helpers/QuestionNavigation.php
+++ application/modules/default/views/helpers/QuestionNavigation.php
@@ -64,7 +64,7 @@ class Module_Default_View_Helper_QuestionNavigation extends Zend_View_Helper_Abs
             $urlParams['qid'] = $item->qi;
             $html .= '<a href="'
                 . $this->view->url($urlParams) . '"'
-                . ($for == 'follow-up-box'? ' class="btn btn-default"'
+                . ($for == 'follow-up-box'? ' class="btn btn-default text-wrap"'
                     : ' class="question-nav-item" data-qid="' . $item->qi . '"')
                 . '>'
                 // Number
