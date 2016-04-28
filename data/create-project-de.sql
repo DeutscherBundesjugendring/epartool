95922763d0c9c65352b3053bdaee8aa11e2930b8
Martin Bohal <martin@visionapps.cz>
2016-04-28 16:57:45 +0200

    DBJR-629: Add translations to email templates

--- data/create-project-de.sql
+++ data/create-project-de.sql
@@ -367,25 +367,55 @@ Eure Koordinierungsstelle'
         'notification_new_follow_up_file_created',
         (SELECT `id` FROM `email_template_type` WHERE `name` = 'system'),
         @project_code,
-        'New follow-up created',
-        'html text email version',
-        'plain text email version'
+        'Neue Reaktion oder Wirkung vorhanden',
+        '<p>Hallo {{to_name}},</p>
+<p>Du bist f&uuml;r die automatische Benachrichtigung bei neuen Reaktionen oder Wirkungen eingetragen. Es gibt Neuigkeiten!</p>
+<p>Um die neuen Reaktionen anzusehen, klicke hier:<br />
+<a href="{{website_url}}">{{website_url}}</a></p>
+<p>Um dich von den automatischen Benachrichtigungen abzumelden, klicke hier:<br />
+<a href="{{unsubscribe_url}}">{{unsubscribe_url}}</a></p>
+<p>Viele Gr&uuml;&szlig;e<br />
+Das Team des ePartool</p>',
+        'Hallo {{to_name}},
+Du bist für die automatische Benachrichtigung bei neuen Reaktionen oder Wirkungen eingetragen. Es gibt Neuigkeiten!
+Um die neuen Reaktionen anzusehen, klicke hier:
+{{website_url}}
+Um dich von den automatischen Benachrichtigungen abzumelden, klicke hier:
+{{unsubscribe_url}}
+Viele Grüße
+Das Team des ePartool'
     ),
     (
         'follow_up_subscription_confirmation',
         (SELECT `id` FROM `email_template_type` WHERE `name` = 'system'),
         @project_code,
-        'Follow up subscription confirmation',
-        'html text email version',
-        'plain text email version'
+        'Bitte bestätige die automatische Benachrichtigung bei neuen Reaktionen',
+        '<p>Hallo {{to_name}},</p>
+<p>bitte best&auml;tige, dass du k&uuml;nftig &uuml;ber neue Reaktionen und Wirkungen zu &bdquo;{{consultation_title_long}}&ldquo; automatisch benachrichtigt werden willst. Klicke hierf&uuml;r auf folgenden Best&auml;tigungslink:<br />
+<a href="{{confirmation_url}}">{{confirmation_url}}</a></p>
+<p>Viele Gr&uuml;&szlig;e<br />
+Das Team des ePartool</p>',
+        '<p>Hallo {{to_name}},</p>
+         bitte bestätige, dass du künftig über neue Reaktionen und Wirkungen zu „{{consultation_title_long}}“ automatisch benachrichtigt werden willst. Klicke hierfür auf folgenden Bestätigungslink:
+         {{confirmation_url}}
+         Viele Grüße
+         Das Team des ePartool'
     ),
     (
         'follow_up_subscription_confirmation_new_user',
         (SELECT `id` FROM `email_template_type` WHERE `name` = 'system'),
         @project_code,
-        'Follow up subscription confirmation for new user',
-        'html text email version',
-        'plain text email version'
+        'Bitte bestätige die Registrierung für automatische Benachrichtigungen über Reaktionen',
+        '<p>Hallo {{to_name}},</p>
+<p>bitte best&auml;tige deine Registrierung als neue_r Empf&auml;nger_in f&uuml;r automatische Benachrichtigungen bei neuen Reaktionen zu &bdquo;{{consultation_title_long}}&ldquo; und den dazugeh&ouml;rigen Nutzeraccount. Klicke hierf&uuml;r auf folgenden Best&auml;tigungslink:<br />
+<a href="{{confirmation_url}}">{{confirmation_url}}</a></p>
+<p>Viele Gr&uuml;&szlig;e<br />
+Das Team des ePartool</p>',
+        'Hallo {{to_name}},
+bitte bestätige deine Registrierung als neue_r Empfänger_in für automatische Benachrichtigungen bei neuen Reaktionen zu „{{consultation_title_long}}“ und den dazugehörigen Nutzeraccount. Klicke hierfür auf folgenden Bestätigungslink:
+{{confirmation_url}}
+Viele Grüße
+Das Team des ePartool'
     );
 
 
