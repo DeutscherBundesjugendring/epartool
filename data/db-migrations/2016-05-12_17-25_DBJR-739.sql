69b5dd0c6a706972fe66cabfec3e7c6a75ab6c6c
Jiri Fous <jiri@visionapps.cz>
2016-05-12 18:28:32 +0200

    DBJR-739: implement db migration for fixing existing vt_rights entires

---
+++ data/db-migrations/2016-05-12_17-25_DBJR-739.sql
@@ -0,0 +1,6 @@
+START TRANSACTION;
+INSERT INTO user_info (SELECT (SELECT '' AS user_info_uid), u.uid, vtr.kid, (SELECT '' AS cmnt), u.source, u.src_misc, u.group_size, u.name_group, u.name_pers, u.age_group, u.regio_pax, u.cnslt_results,
+(SELECT NOW() AS date_added), (SELECT '' AS cmnt_ext), (SELECT NOW() AS time_user_confirmed), NULL, u.name, u.newsl_subscr FROM vt_rights AS vtr INNER JOIN users AS u ON (vtr.uid = u.uid)
+LEFT JOIN user_info AS ui ON (vtr.kid = ui.kid AND u.uid = ui.uid)
+WHERE ui.user_info_id IS NULL);
+COMMIT;
