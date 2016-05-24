3e58d2a6f185c739fa68bc72c71427a655c74705
Jiri Fous <jiri@visionapps.cz>
2016-05-24 12:23:27 +0200

    DBJR-618: add db migration to create-installation

--- data/create-installation.sql
+++ data/create-installation.sql
@@ -917,3 +917,19 @@ SET foreign_key_checks = 1;
 
 -- Migration 2016-05-11_16-03_DBJR-614.sql
 ALTER TABLE `users` ADD `nick` varchar(255) NULL DEFAULT NULL;
+
+-- Migration 2016-05-23_17-52_DBJR-618.sql
+ALTER TABLE `help_text`
+ADD `module` varchar(255) NOT NULL DEFAULT 'default';
+
+ALTER TABLE `help_text`
+ADD UNIQUE `help_text_project_code_name_key` (`project_code`, `name`);
+
+CREATE TABLE `help_text_module` (`name` varchar(255) NOT NULL ); 
+ALTER TABLE `help_text_module` ADD PRIMARY KEY `name` (`name`);
+
+INSERT INTO `help_text_module` (`name`) VALUES ('admin');
+INSERT INTO `help_text_module` (`name`) VALUES ('default');
+
+ALTER TABLE `help_text`
+ADD FOREIGN KEY (`module`) REFERENCES `help_text_module` (`name`) ON DELETE RESTRICT ON UPDATE RESTRICT;
