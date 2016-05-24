c60c2883db3c50e0303e8afe2a3be8ef3bc49a2c
Jiri Fous <jiri@visionapps.cz>
2016-05-24 12:27:59 +0200

    add db migrations to create-installation

--- data/create-installation.sql
+++ data/create-installation.sql
@@ -917,3 +917,24 @@ SET foreign_key_checks = 1;
 
 -- Migration 2016-05-11_16-03_DBJR-614.sql
 ALTER TABLE `users` ADD `nick` varchar(255) NULL DEFAULT NULL;
+
+-- Migration 2016-05-17_14-28_DBJR-608.sql
+ALTER TABLE `proj`
+ADD `video_facebook_enabled` tinyint(1) NOT NULL DEFAULT '0',
+ADD `video_youtube_enabled` tinyint(1) NOT NULL DEFAULT '0' AFTER `video_facebook_enabled`,
+ADD `video_vimeo_enabled` tinyint(1) NOT NULL DEFAULT '0' AFTER `video_youtube_enabled`;
+
+ALTER TABLE `quests`
+ADD `video_enabled` tinyint(1) NOT NULL DEFAULT '0';
+
+ALTER TABLE `inpt`
+ADD `video_service` varchar(255) NULL,
+ADD `video_id` varchar(255) NULL AFTER `video_service`;
+
+-- Migration 2016-05-19-12-27_DBJR-609.sql
+ALTER TABLE `cnslt`
+ADD `discussion_video_enabled` tinyint(1) NOT NULL DEFAULT '1';
+
+-- Migration 2016-05-19_14-18_DBJR-610.sql
+ALTER TABLE `input_discussion`
+ADD `video_service` varchar(255) COLLATE 'utf8_general_ci' NULL AFTER `body`;
