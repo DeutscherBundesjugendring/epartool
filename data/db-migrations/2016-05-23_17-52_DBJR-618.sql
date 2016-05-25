b19e777cff0244603235b6c1567e78c32eaca691
Jiri Fous <jiri@visionapps.cz>
2016-05-25 11:07:09 +0200

    DBJR-618: add admin help-text data to migration

--- data/db-migrations/2016-05-23_17-52_DBJR-618.sql
+++ data/db-migrations/2016-05-23_17-52_DBJR-618.sql
@@ -12,3 +12,80 @@ INSERT INTO `help_text_module` (`name`) VALUES ('default');
 
 ALTER TABLE `help_text`
 ADD FOREIGN KEY (`module`) REFERENCES `help_text_module` (`name`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+INSERT INTO `help_text` (`name`, `body`, `project_code`, `module`)
+(
+    SELECT
+        'help-text-admin-consultation-voting-preparation',
+        'Sample voting-preparation text.',
+        `proj`.`proj`,
+        'admin'
+    FROM
+        proj
+);
+
+INSERT INTO `help_text` (`name`, `body`, `project_code`, `module`)
+(
+    SELECT
+        'help-text-admin-consultation-voting-permissions',
+        'Sample voting-permissions text.',
+        `proj`.`proj`,
+        'admin'
+    FROM
+        proj
+);
+
+INSERT INTO `help_text` (`name`, `body`, `project_code`, `module`)
+(
+    SELECT
+        'help-text-admin-consultation-voting-invitations',
+        'Sample voting-invitations text.',
+        `proj`.`proj`,
+        'admin'
+    FROM
+        proj
+);
+
+INSERT INTO `help_text` (`name`, `body`, `project_code`, `module`)
+(
+    SELECT
+        'help-text-admin-consultation-voting-participants',
+        'Sample voting-participants text.',
+        `proj`.`proj`,
+        'admin'
+    FROM
+        proj
+);
+
+INSERT INTO `help_text` (`name`, `body`, `project_code`, `module`)
+(
+    SELECT
+        'help-text-admin-consultation-voting-results',
+        'Sample voting-results text.',
+        `proj`.`proj`,
+        'admin'
+    FROM
+        proj
+);
+
+INSERT INTO `help_text` (`name`, `body`, `project_code`, `module`)
+(
+    SELECT
+        'help-text-admin-consultation-follow-up',
+        'Sample follow-up text.',
+        `proj`.`proj`,
+        'admin'
+    FROM
+        proj
+);
+
+INSERT INTO `help_text` (`name`, `body`, `project_code`, `module`)
+(
+    SELECT
+        'help-text-admin-consultation-follow-up-snippets',
+        'Sample follow-up-snippets text.',
+        `proj`.`proj`,
+        'admin'
+    FROM
+        proj
+);
