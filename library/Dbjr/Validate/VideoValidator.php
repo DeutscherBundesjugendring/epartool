1d205b6366c4090dfa8192bee0e76ce3e512e50d
Jiri Fous <jiri@visionapps.cz>
2016-05-31 12:35:39 +0200

    DBJR-763: implement api request with php curl

--- library/Dbjr/Validate/VideoValidator.php
+++ library/Dbjr/Validate/VideoValidator.php
@@ -6,7 +6,7 @@ class Dbjr_Validate_VideoValidator extends Zend_Validate_Abstract
     const NOT_PUBLIC = 'not_public';
     
     protected $_messageTemplates = [
-        self::API_UNAVAILABLE => 'Video could not be verified for accessibility.',
+        self::API_UNAVAILABLE => 'Video provider is not currently available. Video could not be verified for accessibility.',
         self::NOT_PUBLIC => 'Video is not public.',
     ];
     
@@ -38,25 +38,28 @@ class Dbjr_Validate_VideoValidator extends Zend_Validate_Abstract
     {
         $appId = Zend_Registry::get('systemconfig')->webservice->facebook->appId;
         $appSecret = Zend_Registry::get('systemconfig')->webservice->facebook->appSecret;
-        
-        $accessToken = @file_get_contents('https://graph.facebook.com/oauth/access_token?client_id=' . $appId
-            . '&client_secret=' . $appSecret . '&grant_type=client_credentials');
 
-        if ($accessToken === FALSE) {
+        $accessToken = $this->sendRequest('https://graph.facebook.com/oauth/access_token', [
+            'client_id' => $appId,
+            'client_secret' => $appSecret,
+            'grant_type' => 'client_credentials',
+        ]);
+
+        if ($accessToken['httpCode'] !== 200) {
             $this->_error(self::API_UNAVAILABLE);
-            return true;
+            return false;
         }
         
-        $jsonResponse = json_decode($accessToken, true);
+        $jsonResponse = json_decode($accessToken['output'], true);
         if ($jsonResponse && isset($jsonResponse['error'])) {
             $this->_error(self::API_UNAVAILABLE);
-            return true;
+            return false;
         }
-        
-        $response = @file_get_contents('https://graph.facebook.com/v2.6/' . $id . '?' . $accessToken);
 
-        if ($response !== FALSE) {
-            $jsonResponse = json_decode($response, true);
+        $response = $this->sendRequest('https://graph.facebook.com/v2.6/' . $id . '?' . $accessToken['output']);
+
+        if ($response['httpCode'] === 200) {
+            $jsonResponse = json_decode($response['output'], true);
             if ($jsonResponse && isset($jsonResponse['id'])) {
                 return true;
             }
@@ -73,17 +76,20 @@ class Dbjr_Validate_VideoValidator extends Zend_Validate_Abstract
     private function validateVimeoVideo($id)
     {
         $accessToken = Zend_Registry::get('systemconfig')->webservice->vimeo->accessToken;
-        $response = @file_get_contents('https://api.vimeo.com/videos/' . $id . '?access_token='.$accessToken);
-        if ($response !== FALSE) {
-            $jsonResponse = json_decode($response, true);
+
+        $response = $this->sendRequest('https://api.vimeo.com/videos/' . $id, ['access_token' => $accessToken]);
+        if ($response['httpCode'] === 200) {
+            $jsonResponse = json_decode($response['output'], true);
             if ($jsonResponse && isset($jsonResponse['privacy']) && isset($jsonResponse['privacy']['embed'])
                 && strpos('public', $jsonResponse['privacy']['embed']) !== false
                 && strpos('anybody', $jsonResponse['privacy']['view']) !== false) {
                 return true;
             }
+            $this->_error(self::NOT_PUBLIC);
+            return false;
         }
-        
-        $this->_error(self::NOT_PUBLIC);
+
+        $this->_error(self::API_UNAVAILABLE);
         return false;
     }
     
@@ -93,11 +99,47 @@ class Dbjr_Validate_VideoValidator extends Zend_Validate_Abstract
      */
     private function validateYoutubeVideo($id)
     {
-        $response = @file_get_contents('https://www.youtube.com/oembed?url=http://www.youtube.com/watch?v=' . $id . '&format=json');
-        if ($response === FALSE) {
+        $response = $this->sendRequest('https://www.youtube.com/oembed', [
+            'url' => 'http://www.youtube.com/watch?v=' . $id,
+            'format' => 'json',
+        ]);
+        
+        if ($response['httpCode'] === 200) {
+            return true;
+        } elseif ($response['httpCode'] === 401) {
             $this->_error(self::NOT_PUBLIC);
             return false;
         }
-        return true;
+
+        $this->_error(self::API_UNAVAILABLE);
+        return false;
+    }
+
+    /**
+     * @param $url
+     * @param array $params
+     * @return array
+     */
+    private function sendRequest($url, array $params = [])
+    {
+        $encoded = '';
+        foreach ($params as $name => $value) {
+            $encoded .= urlencode($name).'='.urlencode($value).'&';
+        }
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url . ($encoded !== '' ? '?' . $encoded : ''));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_FAILONERROR, true);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+        $output = curl_exec($curl);
+        $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+        curl_close($curl);
+        return ['httpCode' => $httpCode, 'output' => $output];
     }
 }
