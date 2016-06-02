af65ae192aab0029add1140ac0118e1cce3a7c22
Jiri Fous <jiri@visionapps.cz>
2016-06-02 12:11:57 +0200

    DBJR-753: display the preview of the document instead of the document

--- assets/front/js/FollowUp.js
+++ assets/front/js/FollowUp.js
@@ -262,7 +262,7 @@
             var _content =
                 '<div class="well well-accent ' + _activeDocClass + '">' +
                 '<div class="row">' +
-                  '<div class="col-sm-3"><img src="' + data.mediafolder + data.doc.ref_doc + '" class="img-responsive pull-left" /></div>' +
+                  '<div class="col-sm-3"><img src="' + data.mediafolder + data.doc.gfx_who + '" class="img-responsive pull-left" /></div>' +
                     '<div class="cols-sm-9">' +
                       '<p>' + data.doc.who + '</p>' +
                       '<p class="small">' + when + '</p>' +
