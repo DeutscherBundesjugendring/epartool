6b7b9f191568f10d5616c5974155f761e2d639a4
Adam Kudrna <adam@visionapps.cz>
2016-06-24 10:51:43 +0200

    DBJR-791: sort colors alphabetically

--- library/Dbjr/Form/Element/RadioTheme.php
+++ library/Dbjr/Form/Element/RadioTheme.php
@@ -10,8 +10,8 @@ class Dbjr_Form_Element_RadioTheme extends Dbjr_Form_Element_Radio
         foreach ($themes as $theme) {
             $themesOptions[$theme['id']] = $theme['name'];
             $colorConfig[$theme['id']] = json_encode([
-                'color_accent_1' => '#' . $theme['color_accent_1'],
                 'color_primary' => '#'. $theme['color_primary'],
+                'color_accent_1' => '#' . $theme['color_accent_1'],
                 'color_accent_2' => '#'. $theme['color_accent_2'],
             ]);
         }
