0bda41f6d9a5ae4872599c83b3a8f64b81991b3d
Martin Bohal <martin@visionapps.cz>
2016-07-12 13:46:25 +0200

    DBJR-837: Polish project removal

--- www/index.php
+++ www/index.php
@@ -2,20 +2,13 @@
 
 $rootPath = dirname(dirname(__FILE__));
 
-define('APPLICATION_ENV', 'development');
-//define('APPLICATION_ENV', 'production');
+//define('APPLICATION_ENV', 'development');
+define('APPLICATION_ENV', 'production');
 define('APPLICATION_PATH', realpath($rootPath . '/application'));
 define('MEDIA_PATH', realpath($rootPath . '/www/media'));
 define('VENDOR_PATH', realpath($rootPath . '/vendor'));
 define('RUNTIME_PATH', realpath($rootPath . '/runtime'));
 
-set_include_path(APPLICATION_PATH);
-require_once(VENDOR_PATH . '/autoload.php');
 require_once(APPLICATION_PATH . '/init.php');
-
-// Enable Tracy for error visualization
-Tracy\Debugger::enable(APPLICATION_ENV === 'development' ? Tracy\Debugger::DEVELOPMENT : Tracy\Debugger::PRODUCTION);
-// Also report E_NOTICE and E_WARNING
-Tracy\Debugger::$strictMode = true;
 
 $application->run();
