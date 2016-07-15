a737efabd36d6d315c908d90acb2147cda5f1767
Jiri Fous <jiri@visionapps.cz>
2016-07-15 12:53:31 +0200

    remove collation

--- data/create-installation.sql
+++ data/create-installation.sql
@@ -1010,9 +1010,9 @@ CHANGE `color_frame_background` `color_primary` varchar(255) NOT NULL AFTER `col
 CHANGE `color_active_link` `color_accent_2` varchar(255) NOT NULL AFTER `color_primary`;
 
 ALTER TABLE `proj`
-CHANGE `color_headings` `color_accent_1` varchar(255) COLLATE 'utf8_general_ci' NULL AFTER `theme_id`,
-CHANGE `color_frame_background` `color_primary` varchar(255) COLLATE 'utf8_general_ci' NULL AFTER `color_accent_1`,
-CHANGE `color_active_link` `color_accent_2` varchar(255) COLLATE 'utf8_general_ci' NULL AFTER `color_primary`;
+CHANGE `color_headings` `color_accent_1` varchar(255) NULL AFTER `theme_id`,
+CHANGE `color_frame_background` `color_primary` varchar(255) NULL AFTER `color_accent_1`,
+CHANGE `color_active_link` `color_accent_2` varchar(255) NULL AFTER `color_primary`;
 
 -- Migration 2016-06-27_18-36_DBJR-761.sql
 ALTER TABLE `proj`
--- data/db-migrations/2016-06-17_16-03_DBJR-626.sql
+++ data/db-migrations/2016-06-17_16-03_DBJR-626.sql
@@ -4,6 +4,6 @@ CHANGE `color_frame_background` `color_primary` varchar(255) NOT NULL AFTER `col
 CHANGE `color_active_link` `color_accent_2` varchar(255) NOT NULL AFTER `color_primary`;
 
 ALTER TABLE `proj`
-CHANGE `color_headings` `color_accent_1` varchar(255) COLLATE 'utf8_general_ci' NULL AFTER `theme_id`,
-CHANGE `color_frame_background` `color_primary` varchar(255) COLLATE 'utf8_general_ci' NULL AFTER `color_accent_1`,
-CHANGE `color_active_link` `color_accent_2` varchar(255) COLLATE 'utf8_general_ci' NULL AFTER `color_primary`;
+CHANGE `color_headings` `color_accent_1` varchar(255) NULL AFTER `theme_id`,
+CHANGE `color_frame_background` `color_primary` varchar(255) NULL AFTER `color_accent_1`,
+CHANGE `color_active_link` `color_accent_2` varchar(255) NULL AFTER `color_primary`;
