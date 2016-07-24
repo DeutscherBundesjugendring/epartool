6ba94321bdf6d3df3e2c02f27ccb0faa709c6368
Martin Bohal <martin@visionapps.cz>
2016-07-24 19:29:52 +0200

    Fix error in license table col name (id vs. number)

--- data/db-migrations/2016-07-14_15-07_DBJR-827.sql
+++ data/db-migrations/2016-07-14_15-07_DBJR-827.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `license` (
-  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `number` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `title` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
   `text` text NOT NULL,
@@ -10,10 +10,10 @@ CREATE TABLE `license` (
 
 ALTER TABLE `proj` ADD `license` int NULL;
 ALTER TABLE `proj` ADD INDEX `proj_license_fkey` (`license`);
-ALTER TABLE `proj` ADD FOREIGN KEY (`license`) REFERENCES `license` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 INSERT INTO `license` (`number`,`title`,`description`,`text`,`link`,`icon`,`alt`) VALUES
 (1, 'Creative commons license', 'Creative Commons license 4.0: attribution, non-commercial', 'The contributions are published under a creative commons license. This means that your contribution may be re-used in summaries and publications for non-commercial use. As all contributions are published anonymously on this page, this website will be referred to as the source when re-using contributions.', 'http://creativecommons.org/licenses/by-nc/4.0/deed.en', 'license_cc.svg','CC-BY-NC 4.0');
 
-UPDATE `proj` SET `license` = (SELECT id FROM `license` WHERE title = 'Creative commons license');
+UPDATE `proj` SET `license` = (SELECT `number` FROM `license` WHERE title = 'Creative commons license');
 ALTER TABLE `proj` CHANGE `license` `license` int(11) NOT NULL;
+ALTER TABLE `proj` ADD FOREIGN KEY (`license`) REFERENCES `license` (`number`) ON DELETE RESTRICT ON UPDATE RESTRICT;
