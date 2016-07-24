d1b552d782f08d49d3f4c802fb56ec0913c8d511
Martin Bohal <martin@visionapps.cz>
2016-07-24 19:29:11 +0200

    Add forgotten semicolons to end of query

--- data/db-migrations/2016-06-23_15-34_DBJR-770.sql
+++ data/db-migrations/2016-06-23_15-34_DBJR-770.sql
@@ -2,4 +2,4 @@ ALTER TABLE `vt_grps`
 ADD INDEX `vt_grps_sub_uid_fkey` (`sub_uid`);
 
 ALTER TABLE `vt_indiv`
-ADD FOREIGN KEY (`sub_uid`) REFERENCES `vt_grps` (`sub_uid`) ON DELETE RESTRICT ON UPDATE RESTRICT
+ADD FOREIGN KEY (`sub_uid`) REFERENCES `vt_grps` (`sub_uid`) ON DELETE RESTRICT ON UPDATE RESTRICT;
