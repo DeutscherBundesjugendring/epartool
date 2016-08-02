50fb2ef241cda1085695d58c4a52edd3aefb91a9
Martin Bohal <martin@visionapps.cz>
2016-08-02 14:50:57 +0200

    DBJR-888: Use dev env based on domain and ignore installer in dev env.

--- www/index.php
+++ www/index.php
@@ -1,14 +1,8 @@
 <?php
 
 $rootPath = dirname(dirname(__FILE__));
-
-//define('APPLICATION_ENV', 'development');
 define('APPLICATION_ENV', 'production');
-define('MEDIA_URL', '/www/media');
 define('APPLICATION_PATH', realpath($rootPath . '/application'));
-define('MEDIA_PATH', realpath($rootPath . '/' . MEDIA_URL));
-define('VENDOR_PATH', realpath($rootPath . '/vendor'));
-define('RUNTIME_PATH', realpath($rootPath . '/runtime'));
 
 if (!realpath(dirname(__FILE__) . '/../application/configs/config.local.ini')) {
     $url = sprintf(
