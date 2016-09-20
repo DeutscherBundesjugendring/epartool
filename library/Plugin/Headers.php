32006db19a5fe3d73beba6cbe88b54baa82fa8cc
Jiri Fous <jiri@visionapps.cz>
2016-09-20 17:04:20 +0200

    DBJR-945: add X-Content-Type-Options header

--- library/Plugin/Headers.php
+++ library/Plugin/Headers.php
@@ -8,6 +8,7 @@ class Plugin_Headers extends Zend_Controller_Plugin_Abstract
     public function postDispatch(Zend_Controller_Request_Abstract $request)
     {
         $this->getResponse()
-            ->setHeader('Content-Security-Policy', "default-src 'self'; media-src youtube.com vimeo.com facebook.com;");
+            ->setHeader('Content-Security-Policy', "default-src 'self'; media-src youtube.com vimeo.com facebook.com;")
+            ->setHeader('X-Content-Type-Options', "nosniff");
     }
 }
