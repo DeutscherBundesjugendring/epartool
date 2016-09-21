f6448345ad9678e83ede56f3d8109df5a814f243
Jiri Fous <jiri@visionapps.cz>
2016-09-21 18:24:03 +0200

    fixup! DBJR-945: add http cookie only option to zend sessions settings

--- library/Plugin/Headers.php
+++ library/Plugin/Headers.php
@@ -8,7 +8,13 @@ class Plugin_Headers extends Zend_Controller_Plugin_Abstract
     public function postDispatch(Zend_Controller_Request_Abstract $request)
     {
         $this->getResponse()
-            ->setHeader('Content-Security-Policy', "default-src 'self'; media-src youtube.com vimeo.com facebook.com;")
+            ->setHeader(
+                'Content-Security-Policy',
+                "default-src 'self';
+                 media-src *.youtube.com *.ytimg.com *.vimeo.com *.facebook.com *.facebook.net *.twitter.com;
+                 font-src gstatic.com
+                 script-src *.google.com *.youtube.com *.facebook.com *.doubleclick.net *.twitter.com gstatic.com;"
+            )
             ->setHeader('X-Content-Type-Options', "nosniff");
     }
 }
