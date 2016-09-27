3b1750d65ae4e3d4ac2e8c5813b64cbb472a8cbb
Jiri Fous <jiri@visionapps.cz>
2016-09-27 19:48:53 +0200

    DBJR-969: add codeception acceptance tests, phantomjs

---
+++ tests/acceptance/default/IndexControllerCest.php
@@ -0,0 +1,19 @@
+<?php
+
+class IndexControllerCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+        $I->amOnPage('/');
+    }
+
+    public function _after(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+        $I->see('participation');
+    }
+}
