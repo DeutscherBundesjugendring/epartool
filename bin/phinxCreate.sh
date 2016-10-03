f3ab7283d79e43ce4901c540486da9291e45611e
Jiri Fous <jiri@visionapps.cz>
2016-10-03 11:02:31 +0200

    Merge branch 'develop' of ssh://bitbucket.visionapps.cz:7999/dbjr/tool into feature/DBJR-966

--- bin/phinxCreate.sh
+++ bin/phinxCreate.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 
-vendor/bin/phinx create $1  -c application/configs/phinx.local.yml \
-                            --template="data/phinx-migrations/phinx_migration.php.dist"
+vendor/bin/phinx create $1 \
+    -c application/configs/phinx.local.yml \
+    --template="data/phinx-migrations/phinx_migration.php.dist"
