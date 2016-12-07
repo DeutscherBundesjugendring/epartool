0d92e0afdaf243085ff0af286f50d66ba010c274
Jiri Fous <jiri@visionapps.cz>
2016-12-07 15:18:57 +0100

    DBJR-1011: reimplement version info in admin

--- application/views/helpers/ToolVersion.php
+++ application/views/helpers/ToolVersion.php
@@ -5,12 +5,30 @@
  */
 class Application_View_Helper_ToolVersion extends Zend_View_Helper_Abstract
 {
+    const VERSION_FILE = 'VERSION.txt';
+
+    /**
+     * @var string
+     */
+    private $versionInfo;
+
     /**
      * @return string
      */
     public function toolVersion()
     {
-        $version = (new Zend_Registry())->get('systemconfig')->version;
-        return $version !== null ? $version : 0;
+        if ($this->versionInfo === null) {
+            $versionFilePath = APPLICATION_PATH . '/../' . self::VERSION_FILE;
+            if (!file_exists($versionFilePath) || !($content = file_get_contents($versionFilePath))) {
+                trigger_error('No valid file VERSION.txt found in app root.', E_USER_WARNING);
+                $this->versionInfo = 0;
+
+                return $this->versionInfo;
+            }
+
+            $this->versionInfo = trim($content);
+        }
+
+        return $this->versionInfo;
     }
 }
