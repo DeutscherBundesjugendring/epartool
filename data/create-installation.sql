2dd0516b183f6f7592d6f08a0cd3b2f33b140f4e
Jiri Fous <jiri@visionapps.cz>
2017-01-30 11:10:40 +0100

    add missing migration into create installation sql script

--- data/create-installation.sql
+++ data/create-installation.sql
@@ -1276,4 +1276,23 @@ ALTER TABLE `user_info` ADD `invitation_sent_date` datetime NULL DEFAULT NULL;
 ALTER TABLE `vt_settings` ADD `btn_no_opinion` boolean NOT NULL DEFAULT true AFTER `btn_important`;
 
 -- Migration 20170116123840_dbjr1072.php
-ALTER TABLE `cnslt` CHANGE `img_file` `img_file` text NULL AFTER `titl_sub`
+ALTER TABLE `cnslt` CHANGE `img_file` `img_file` text NULL AFTER `titl_sub`;
+
+-- Migration 2016-05-24_10-57_DBJR-608.sql
+CREATE TABLE `video_service` (`name` varchar(255) NOT NULL );
+ALTER TABLE `video_service` ADD PRIMARY KEY `name` (`name`);
+INSERT INTO `video_service` (`name`) VALUES ('vimeo');
+INSERT INTO `video_service` (`name`) VALUES ('youtube');
+INSERT INTO `video_service` (`name`) VALUES ('facebook');
+
+ALTER TABLE `inpt`
+ADD INDEX `inpt_video_service_fkey` (`video_service`);
+
+ALTER TABLE `input_discussion`
+ADD INDEX `input_discussion_video_service_fkey` (`video_service`);
+
+ALTER TABLE `input_discussion`
+ADD FOREIGN KEY (`video_service`) REFERENCES `video_service` (`name`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+ALTER TABLE `inpt`
+ADD FOREIGN KEY (`video_service`) REFERENCES `video_service` (`name`) ON DELETE RESTRICT ON UPDATE RESTRICT;
