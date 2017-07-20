06be974f28390999b508aec492216223e4b2ac7e
Jiri Fous <jiri@visionapps.cz>
2017-07-20 11:52:10 +0200

    DBJR-1203: fix download file in FireFox

--- assets/front/react/src/service/downloadFile.jsx
+++ assets/front/react/src/service/downloadFile.jsx
@@ -1,8 +1,14 @@
 const downloadFile = (url) => {
+  const event = new MouseEvent('click', {
+    'view': window,
+    'bubbles': true,
+    'cancelable': true
+  });
+
   const link = document.createElement('a');
   link.href = url;
   link.download = url.split('/').pop();
-  link.click();
+  link.dispatchEvent(event);
 };
 
 export default downloadFile;
