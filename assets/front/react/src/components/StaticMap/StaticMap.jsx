b2d8547dc56d4a0e1468021e6773f0eff8faa57f
Adam Kudrna <adam@visionapps.cz>
2017-12-22 22:42:28 +0100

    DBJR-1300: style maps and map views in frontend

--- assets/front/react/src/components/StaticMap/StaticMap.jsx
+++ assets/front/react/src/components/StaticMap/StaticMap.jsx
@@ -3,17 +3,19 @@ import React from 'react';
 /* global osmStaticMapUrlTemplate */
 
 const StaticMap = props => (
-  <img
-    width={props.width}
-    height={props.height}
-    src={
-      osmStaticMapUrlTemplate.replace(/__latitude__/g, props.latitude)
-        .replace(/__longitude__/g, props.longitude)
-        .replace('__width__', props.width)
-        .replace('__height__', props.height)
-    }
-    alt={`GPS: ${props.latitude}, ${props.longitude}`}
-  />
+  <div className="map-static map-static-landscape">
+    <img
+      width={props.width}
+      height={props.height}
+      src={
+        osmStaticMapUrlTemplate.replace(/__latitude__/g, props.latitude)
+          .replace(/__longitude__/g, props.longitude)
+          .replace('__width__', props.width)
+          .replace('__height__', props.height)
+      }
+      alt={`GPS: ${props.latitude}, ${props.longitude}`}
+    />
+  </div>
 );
 
 StaticMap.defaultProps = {
